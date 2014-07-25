795c86018a842d90012ab005ee73adf2e64eca8e
Olivier Nouguier <olivier.nouguier@geomatys.com>
2014-07-25 10:24:56 +0200

    Refactor project layout. Add core/store project

---
+++ core/sirs-core-store/src/main/java/com/geomatys/jooq/converter/CalendarConverter.java
@@ -0,0 +1,37 @@
+package com.geomatys.jooq.converter;
+
+import java.sql.Timestamp;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import org.jooq.Converter;
+
+public class CalendarConverter implements Converter<Timestamp, Calendar> {
+
+    /**
+     * Serialization UID
+     */
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    public Calendar from(Timestamp databaseObject) {
+        GregorianCalendar calendar = (GregorianCalendar) GregorianCalendar.getInstance();
+        calendar.setTimeInMillis(databaseObject.getTime());
+        return calendar;
+    }
+
+    @Override
+    public Timestamp to(Calendar userObject) {
+        return new Timestamp(userObject.getTime().getTime());
+    }
+
+    @Override
+    public Class<Timestamp> fromType() {
+        return Timestamp.class;
+    }
+
+    @Override
+    public Class<Calendar> toType() {
+        return Calendar.class;
+    }
+}
