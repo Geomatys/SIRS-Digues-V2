8eb347c04d0c5f24be37268bb3738e497dda43fa
Olivier Nouguier <olivier.nouguier@geomatys.com>
2014-07-25 16:55:01 +0200

    Use liquidbase. Add support for property changes (javabean)

--- desktop/src/main/java/fr/sym/digue/DiguePlugin.java
+++ desktop/src/main/java/fr/sym/digue/DiguePlugin.java
@@ -2,8 +2,25 @@
 
 package fr.sym.digue;
 
-import fr.sym.Symadrem;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import liquibase.Liquibase;
+import liquibase.database.Database;
+import liquibase.database.DatabaseFactory;
+import liquibase.database.jvm.JdbcConnection;
+import liquibase.exception.LiquibaseException;
+import liquibase.resource.ClassLoaderResourceAccessor;
+
+import org.geotoolkit.util.FileUtilities;
+import org.h2.jdbcx.JdbcConnectionPool;
+
 import fr.sym.Plugin;
+import fr.sym.Symadrem;
 import fr.sym.digue.theme.DisorderTheme;
 import fr.sym.digue.theme.DocumentsTheme;
 import fr.sym.digue.theme.FreeBordTheme;
@@ -15,14 +32,6 @@ import fr.sym.digue.theme.SeaWorkTheme;
 import fr.sym.digue.theme.StructureTheme;
 import fr.sym.digue.theme.TownAreaTheme;
 import fr.sym.digue.theme.WorksNetwork;
-import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DatabaseMetaData;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import org.geotoolkit.util.FileUtilities;
-import org.h2.jdbcx.JdbcConnectionPool;
 
 /**
  *
@@ -50,29 +59,43 @@ public class DiguePlugin extends Plugin{
         //check that table exist
         final JdbcConnectionPool pool = Symadrem.getConnectionPool();
         try(final Connection cnx = pool.getConnection()){
-        
-            final DatabaseMetaData metadata = cnx.getMetaData();
-            ResultSet rs = metadata.getTables(null, null, "DIGUE", null);
-            final boolean loaded = rs.next();
-            rs.close();
-
-            if(!loaded){
-                //run table creation script
-                final Statement stmt = cnx.createStatement();
-                String script = FileUtilities.getStringFromStream(DiguePlugin.class.getResourceAsStream("/fr/sym/db/ADdbDonnees_create.sql"));
-                //remove comments
-                script = script.replaceAll("--.*\n", "");
-                
-                final String[] parts = script.split(";");
-                for(String str : parts){
-                    str = str.trim();
-                    if(str.isEmpty()) continue;
-                    stmt.executeUpdate(str);
+            Liquibase liquibase = null;
+                Database database;
+                try {
+                    database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(new JdbcConnection(cnx));
+                    liquibase = new Liquibase("fr/symadrem/sirs/db/db-changelog.xml", new ClassLoaderResourceAccessor(), database);
+                    liquibase.update("");
+                    database.close();
+                } catch (LiquibaseException e) {
+                    e.printStackTrace();
                 }
-                stmt.close();
-                cnx.commit();
-            }
             
+            
+//                _old(cnx);
+        }
+    }
+
+    private void _old(final Connection cnx) throws SQLException, IOException {
+        final DatabaseMetaData metadata = cnx.getMetaData();
+        ResultSet rs = metadata.getTables(null, null, "DIGUE", null);
+        final boolean loaded = rs.next();
+        rs.close();
+
+        if(!loaded){
+            //run table creation script
+            final Statement stmt = cnx.createStatement();
+            String script = FileUtilities.getStringFromStream(DiguePlugin.class.getResourceAsStream("/fr/sym/db/ADdbDonnees_create.sql"));
+            //remove comments
+            script = script.replaceAll("--.*\n", "");
+            
+            final String[] parts = script.split(";");
+            for(String str : parts){
+                str = str.trim();
+                if(str.isEmpty()) continue;
+                stmt.executeUpdate(str);
+            }
+            stmt.close();
+            cnx.commit();
         }
     }
     
