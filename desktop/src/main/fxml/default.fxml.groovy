cedd678678bf3a2f56dd27128e43718236bc73a6
Olivier Nouguier <olivier.nouguier@geomatys.com>
2014-08-11 16:36:06 +0200

    Version 0.0.3 of UI generator, generated only non overridden fxml files.

--- desktop/src/main/fxml/default.fxml.groovy
+++ desktop/src/main/fxml/default.fxml.groovy
@@ -4,10 +4,10 @@
 <?import java.lang.*?>
 <?import javafx.scene.layout.*?>
 
-<GridPane maxHeight="-Infinity" maxWidth="-Infinity" minHeight="-Infinity" minWidth="-Infinity" prefHeight="400.0" prefWidth="600.0" xmlns="http://javafx.com/javafx/8" xmlns:fx="http://javafx.com/fxml/1">
+<fx:root hgap="10.0" vgap="10.0" maxHeight="1.7976931348623157E308" maxWidth="1.7976931348623157E308" minHeight="-Infinity" minWidth="-Infinity" type="GridPane" xmlns="http://javafx.com/javafx/8" xmlns:fx="http://javafx.com/fxml/1">
   <columnConstraints>
-    <ColumnConstraints hgrow="SOMETIMES" minWidth="10.0" prefWidth="100.0" />
-    <ColumnConstraints hgrow="SOMETIMES" minWidth="10.0" prefWidth="100.0" />
+    <ColumnConstraints hgrow="SOMETIMES" minWidth="10.0" />
+    <ColumnConstraints hgrow="SOMETIMES" maxWidth="1.7976931348623157E308" minWidth="10.0" />
   </columnConstraints>
   <rowConstraints>
     <RowConstraints minHeight="10.0" prefHeight="30.0" vgrow="SOMETIMES" />
@@ -26,4 +26,4 @@
       <Label fx:id="${field.name}Label" <% if(i>0){ %>  GridPane.rowIndex="<%= i %>" <% } %> text="${field.name}" />      
       <${control} fx:id="${field.name}${control}" <% if(i>0){ %>  GridPane.rowIndex="${i}" <% } %> GridPane.columnIndex="1" /><% } %>
    </children>
-</GridPane>
+</fx:root>
