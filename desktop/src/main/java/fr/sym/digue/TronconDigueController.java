5d5adbdc828c4219dd33d3a5be68298526bf210f
Samuel Andrés <samuel.andres@geomatys.com>
2014-10-03 10:06:00 +0200

    Update TronconDigue view with digue label list.

--- desktop/src/main/java/fr/sym/digue/TronconDigueController.java
+++ desktop/src/main/java/fr/sym/digue/TronconDigueController.java
@@ -2,16 +2,28 @@
 
 package fr.sym.digue;
 
+import fr.sym.Session;
 import fr.sym.Symadrem;
+import fr.symadrem.sirs.core.model.Digue;
 import fr.symadrem.sirs.core.model.TronconDigue;
 import java.io.IOException;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.util.List;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleButton;
+import javafx.util.StringConverter;
+import jidefx.scene.control.combobox.LocalDateComboBox;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  *
@@ -22,11 +34,23 @@ public class TronconDigueController {
     public Parent root;
     private TronconDigue troncon;
     
+    @Autowired
+    private Session session;
+    
     @FXML
     private TextField section_name;
     
     @FXML
     private TextArea commentaireTronconTextField;
+    
+    @FXML
+    private ChoiceBox<Digue> digues;
+    
+    @FXML
+    private DatePicker date_debut;
+    
+    @FXML
+    private DatePicker date_fin;
     
     @FXML
     private ToggleButton editionButton;
@@ -51,6 +75,37 @@ public class TronconDigueController {
         this.commentaireTronconTextField.setEditable(true);
         this.commentaireTronconTextField.setWrapText(true);
         this.commentaireTronconTextField.textProperty().bindBidirectional(this.troncon.commentaireProperty());
+        
+        //this.digues.
+        
+        final List<Digue> digs = session.getDigues();
+        final ObservableList<Digue> diguesObservables = FXCollections.observableArrayList();
+        digs.stream().forEach((dig) -> {
+            diguesObservables.add(dig);
+        });
+        this.digues.setItems(diguesObservables);
+        StringConverter<Digue> digueStringConverter = new StringConverter<Digue>() {
+        @Override
+        public String toString(Digue digue) {return digue.getLibelle();}
+
+        // TODO
+        @Override
+        public Digue fromString(String string) {return null;}
+        };
+        this.digues.setConverter(digueStringConverter);
+        //this.digues.setEditable(false);
+        
+        
+        
+        /////////////////////////////////////// A VOIR /////////////////////////
+        this.date_debut.valueProperty();
+        
+        Instant instant = Instant.now();
+        //instant.
+        LocalDate date = this.date_debut.getValue();
+        
+        
+        //LocalDate d = new LocalDate();
     }
     
     public static TronconDigueController create(TronconDigue troncon) {
@@ -62,7 +117,9 @@ public class TronconDigueController {
         } catch (IOException ex) {
             throw new IllegalArgumentException(ex.getMessage(), ex);
         }
+        
         final TronconDigueController controller = loader.getController();
+        Injector.injectDependencies(controller);
         controller.root = root;
         controller.init(troncon);
         return controller;
