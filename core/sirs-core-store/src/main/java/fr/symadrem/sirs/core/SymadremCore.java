79e83d06131a31f728759d1c4ec37c87729f7304
Olivier Nouguier <olivier.nouguier@geomatys.com>
2014-10-24 11:34:50 +0200

    Add support for notification.

---
+++ core/sirs-core-store/src/main/java/fr/symadrem/sirs/core/SymadremCore.java
@@ -0,0 +1,12 @@
+package fr.symadrem.sirs.core;
+
+import java.util.logging.Logger;
+
+import org.apache.sis.util.logging.Logging;
+
+
+public class SymadremCore {
+
+	 public static final Logger LOGGER = Logging.getLogger(SymadremCore.class);
+	
+}
