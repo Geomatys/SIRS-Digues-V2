c7305b2c426cc7beddeb2ca2a3c9fb6ef7683104
Johann Sorel <johann.sorel@geomatys.com>
2014-12-08 15:29:16 +0100

    Search : new search panels

---
+++ desktop/src/main/java/fr/sirs/query/FXSQLFilterEditor.java
@@ -0,0 +1,216 @@
+
+package fr.sirs.query;
+
+import static fr.sirs.SIRS.CSS_PATH;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.geometry.VPos;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.RowConstraints;
+import org.geotoolkit.display2d.GO2Utilities;
+import org.opengis.filter.Filter;
+import org.opengis.filter.FilterFactory2;
+import org.opengis.filter.expression.Literal;
+import org.opengis.filter.expression.PropertyName;
+
+/**
+ *
+ * @author Johann Sorel (Geomatys)
+ */
+public class FXSQLFilterEditor extends GridPane {
+
+    private static final FilterFactory2 FF = GO2Utilities.FILTER_FACTORY;
+    private static final int SPACING = 5;
+    private static final Image DIV_HAUT = new Image("/fr/sirs/div_haut.png");
+    private static final Image DIV_BAS = new Image("/fr/sirs/div_bas.png");
+    
+    public static enum Type{
+        NONE("-"),
+        PROPERTY("PROP"),
+        AND("ET"),
+        OR("OU");
+
+        private final String text;
+
+        private Type(String text) {
+            this.text = text;
+        }
+        
+        @Override
+        public String toString() {
+            return text;
+        }
+    }
+    public static enum Condition{
+        EQUAL("="),
+        LIKE("LIKE"),
+        SUPERIOR(">"),
+        INFERIOR("<"),
+        SUPERIOR_EQUAL(">="),
+        INFERIOR_EQUAL("<=");
+        
+        private final String text;
+
+        private Condition(String text) {
+            this.text = text;
+        }
+        
+        @Override
+        public String toString() {
+            return text;
+        }
+    }
+    
+    private final ChoiceBox<Type> uiTypeBox = new ChoiceBox<>(FXCollections.observableArrayList(Type.values()));
+    private final ChoiceBox<Condition> uiConditionBox = new ChoiceBox<>(FXCollections.observableArrayList(Condition.values()));
+    private final TextField uiPropertyName = new TextField();
+    private final TextField uiPropertyValue = new TextField();
+    private final GridPane uiPropertyPane = new GridPane();
+    
+    private FXSQLFilterEditor uiSub1 = null;
+    private FXSQLFilterEditor uiSub2 = null;
+    
+    public FXSQLFilterEditor() {
+        this(Type.NONE);
+    }
+    
+    public FXSQLFilterEditor(Type type) {
+        getStylesheets().add(CSS_PATH);
+        alignmentProperty().set(Pos.CENTER_LEFT);
+        setHgap(0);
+        setVgap(0);
+        //setGridLinesVisible(true);
+        
+        getColumnConstraints().add(new ColumnConstraints());
+        getColumnConstraints().add(new ColumnConstraints());
+        getColumnConstraints().add(new ColumnConstraints());
+        
+        final RowConstraints rc1 = new RowConstraints();
+        rc1.setVgrow(Priority.ALWAYS);
+        rc1.setValignment(VPos.BOTTOM);
+        final RowConstraints rc2 = new RowConstraints();
+        rc2.setMinHeight(15);
+        rc2.setVgrow(Priority.NEVER);
+        final RowConstraints rc3 = new RowConstraints();
+        rc3.setMinHeight(15);
+        rc3.setVgrow(Priority.NEVER);
+        final RowConstraints rc4 = new RowConstraints();
+        rc4.setVgrow(Priority.ALWAYS);
+        rc4.setValignment(VPos.TOP);
+        
+        getRowConstraints().add(rc1);
+        getRowConstraints().add(rc2);
+        getRowConstraints().add(rc3);
+        getRowConstraints().add(rc4);
+        
+        uiTypeBox.valueProperty().addListener(this::typeChanged);
+        uiTypeBox.getSelectionModel().select(Type.NONE);
+        uiTypeBox.getStyleClass().add("btn-gray");
+        add(uiTypeBox,0,1,1,2);
+        
+        uiConditionBox.getStyleClass().add("btn-gray");
+        uiConditionBox.setValue(Condition.EQUAL);
+        
+        uiPropertyPane.setHgap(SPACING);
+        uiPropertyPane.setVgap(SPACING);
+        uiPropertyPane.setPadding(new Insets(SPACING, SPACING, SPACING, SPACING));
+        uiPropertyPane.getColumnConstraints().add(new ColumnConstraints());
+        uiPropertyPane.getColumnConstraints().add(new ColumnConstraints());
+        uiPropertyPane.getColumnConstraints().add(new ColumnConstraints());
+        uiPropertyPane.getRowConstraints().add(new RowConstraints());
+        uiPropertyPane.add(uiPropertyName, 0, 0);
+        uiPropertyPane.add(uiConditionBox, 1, 0);
+        uiPropertyPane.add(uiPropertyValue, 2, 0);
+    }
+    
+    private void typeChanged(ObservableValue<? extends Type> observable, Type oldValue, Type newValue){
+        while(getChildren().size()>1){
+            getChildren().remove(1);
+        }
+        
+        if(Type.PROPERTY.equals(newValue)){
+            add(uiPropertyPane, 1, 1, 1, 2);
+        }else if(Type.AND.equals(newValue) || Type.OR.equals(newValue)){
+            uiSub1 = new FXSQLFilterEditor();
+            uiSub2 = new FXSQLFilterEditor();
+//            final GridPane pane = new GridPane();
+//            final RowConstraints rc1 = new RowConstraints();
+//            final RowConstraints rc2 = new RowConstraints();
+//            rc1.setFillHeight(true);
+//            rc2.setFillHeight(true);
+//            pane.getRowConstraints().add(rc1);
+//            pane.getRowConstraints().add(rc2);
+//            pane.getColumnConstraints().add(new ColumnConstraints());
+//            pane.getColumnConstraints().add(new ColumnConstraints());
+//            pane.add(uiSub1, 1, 0);
+//            pane.add(uiSub2, 1, 1);
+            add(uiSub1,2,0,1,2);  
+            add(uiSub2,2,2,1,2);
+            
+            
+            final ImageView ivhaut = new ImageView(DIV_HAUT);
+            final ScrollPane scrollhaut = new ScrollPane(ivhaut);
+            scrollhaut.getStyleClass().add("btn-without-style");
+            scrollhaut.setMaxHeight(Double.MAX_VALUE);
+            scrollhaut.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+            scrollhaut.setVbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+            scrollhaut.setPrefWidth(20);
+            scrollhaut.setPrefHeight(1);
+            ivhaut.fitHeightProperty().bind(scrollhaut.heightProperty());
+            add(scrollhaut,1,0,1,2);  
+            
+            final ImageView ivbas = new ImageView(DIV_BAS);
+            final ScrollPane scrollbas = new ScrollPane(ivbas);
+            scrollbas.getStyleClass().add("btn-without-style");
+            scrollbas.setMaxHeight(Double.MAX_VALUE);
+            scrollbas.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+            scrollbas.setVbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+            scrollbas.setPrefWidth(20);
+            scrollbas.setPrefHeight(1);
+            ivbas.fitHeightProperty().bind(scrollbas.heightProperty());
+            add(scrollbas,1,2,1,2);
+        }
+    }
+    
+    public Filter toFilter(){
+        
+        final Type t = uiTypeBox.getValue();
+        if(Type.PROPERTY.equals(t)){
+            final PropertyName propName = FF.property(uiPropertyName.getText());
+            final Literal propValue = FF.literal(uiPropertyValue.getText());
+            
+            final Condition condition = uiConditionBox.getValue();
+            if(Condition.INFERIOR.equals(condition)){
+                return FF.less(propName, propValue);
+            }else if(Condition.INFERIOR_EQUAL.equals(condition)){
+                return FF.lessOrEqual(propName, propValue);
+            }else if(Condition.SUPERIOR.equals(condition)){
+                return FF.greater(propName, propValue);
+            }else if(Condition.SUPERIOR_EQUAL.equals(condition)){
+                return FF.greaterOrEqual(propName, propValue);
+            }else if(Condition.LIKE.equals(condition)){
+                return FF.like(propName, String.valueOf(propValue.getValue()));
+            }else{
+                return FF.equals(propName, propValue);
+            }
+            
+        }else if(Type.AND.equals(t)){
+            return FF.and(uiSub1.toFilter(), uiSub2.toFilter());
+        }else if(Type.OR.equals(t)){
+            return FF.or(uiSub1.toFilter(), uiSub2.toFilter());
+        }else{
+            return Filter.INCLUDE;
+        }
+    }
+    
+    
+}
--- desktop/src/main/java/fr/sirs/query/FXSearchPane.java
+++ desktop/src/main/java/fr/sirs/query/FXSearchPane.java
@@ -4,26 +4,28 @@ package fr.sirs.query;
 import fr.sirs.Injector;
 import fr.sirs.SIRS;
 import fr.sirs.Session;
-import fr.sirs.core.model.Element;
 import fr.sirs.index.ElasticSearchEngine;
 import fr.sirs.theme.ui.PojoTable;
 import fr.sirs.util.SirsTableCell;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.ListView;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Toggle;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.util.Callback;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.Client;
@@ -38,8 +40,25 @@ import org.elasticsearch.search.SearchHits;
  */
 public class FXSearchPane extends BorderPane {
 
+    @FXML private BorderPane uiTopPane;
     @FXML private TableView<ElementHit> uiTable;
-    @FXML private TextField uiSearchField;
+    
+    //Recherche plein texte
+    @FXML private ToggleButton uiTogglePlainText;
+    @FXML private GridPane uiPlainTextPane;
+    @FXML private TextField uiElasticKeywords;
+    
+    //recherche simple
+    @FXML private ToggleButton uiToggleSimple;
+    @FXML private GridPane uiSimplePane;
+    @FXML private ChoiceBox<?> uiDBTable;
+    @FXML private BorderPane uiFilterPane;
+    private FXSQLFilterEditor uiFilterEditor;
+    
+    //recherche SQL
+    @FXML private ToggleButton uiToggleSQL;
+    @FXML private GridPane uiSQLPane;
+    @FXML private TextArea uiSQL;
     
     private final Session session;
 
@@ -78,14 +97,47 @@ public class FXSearchPane extends BorderPane {
         uiTable.getColumns().add(editCol);
         uiTable.getColumns().add(typeCol);
         uiTable.getColumns().add(libelleCol);
+        
+        uiFilterEditor = new FXSQLFilterEditor();
+        uiFilterPane.setCenter(uiFilterEditor);
+        
+        uiTopPane.setCenter(null);
+        
+        //affichage des panneaux coord/borne
+        final ToggleGroup group = new ToggleGroup();
+        uiTogglePlainText.setToggleGroup(group);
+        uiToggleSimple.setToggleGroup(group);
+        uiToggleSQL.setToggleGroup(group);
+        group.selectedToggleProperty().addListener((ObservableValue<? extends Toggle> observable, Toggle oldValue, Toggle newValue) -> {
+            if(newValue==null){
+                group.selectToggle(uiTogglePlainText);
+            }
+            if(newValue==uiTogglePlainText){
+                uiTopPane.setCenter(uiPlainTextPane);
+            }else if(newValue==uiToggleSimple){
+                uiTopPane.setCenter(uiSimplePane);
+            }else if(newValue==uiToggleSQL){
+                uiTopPane.setCenter(uiSQLPane);
+            }else{
+                uiTopPane.setCenter(null);
+            }
+        });
+        
+        uiTogglePlainText.setSelected(true);
+
     }
 
+    @FXML
+    private void viewDBModel(ActionEvent event) {
+        
+    }
+        
     @FXML
     private void search(ActionEvent event) {
         
         final ElasticSearchEngine engine = Injector.getElasticEngine();        
         final Client client = engine.getClient();
-        final QueryBuilder qb = QueryBuilders.queryString(uiSearchField.getText());
+        final QueryBuilder qb = QueryBuilders.queryString(uiElasticKeywords.getText());
         
         final SearchResponse response = client.prepareSearch("sirs")
                 .setQuery(qb)
