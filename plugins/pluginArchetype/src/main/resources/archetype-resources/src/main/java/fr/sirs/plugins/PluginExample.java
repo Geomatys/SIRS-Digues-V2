65857f24a30fc65459479760d6ceae03c8728be5
Cédric Briançon <cedric.briancon@geomatys.com>
2015-06-09 12:30:23 +0200

    feat(archetype): add example of menu buttons for plugin

--- plugins/pluginArchetype/src/main/resources/archetype-resources/src/main/java/fr/sirs/plugins/PluginExample.java
+++ plugins/pluginArchetype/src/main/resources/archetype-resources/src/main/java/fr/sirs/plugins/PluginExample.java
@@ -1,44 +1,37 @@
 package fr.sirs.plugins;
 
 import fr.sirs.Plugin;
-import fr.sirs.theme.Theme;
-import javafx.scene.Parent;
-import javafx.scene.control.Tab;
-import javafx.scene.control.TabPane;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.image.Image;
 
 /**
  * Minimal example of a plugin.
  *
  * @author Alexis Manin (Geomatys)
+ * @author Cédric Briançon (Geomatys)
  */
 public class PluginExample extends Plugin {
+    private static final String NAME = "plugin-example";
+    private static final String TITLE = "Module exemple";
 
     public PluginExample() {
-        loadingMessage.set("I'm an information text. I'm displayed when the plugin is loaded at application initialisation.");
-        themes.add(new Theme("PluginExample theme", Theme.Type.OTHER) {
-            
-            @Override
-            public Parent createPane() {
-                final BorderPane uiCenter = new BorderPane();
-                final TabPane tabPane = new TabPane();
-
-                final Tab tabIntervenant = new Tab("first tab");
-                final Tab tabOrganisme = new Tab("second tab");
-
-                tabPane.getTabs().add(tabIntervenant);
-                tabPane.getTabs().add(tabOrganisme);
-
-                uiCenter.setCenter(tabPane);
-                return tabPane;
-            }
-        });
+        name = NAME;
+        loadingMessage.set("Chargement du module d'exemple");
+        themes.add(new ButtonExampleTheme());
     }
 
     @Override
     public void load() throws Exception {
-        getDescription();
+        getConfiguration();
     }
-    
-    
+
+    @Override
+    public CharSequence getTitle() {
+        return TITLE;
+    }
+
+    @Override
+    public Image getImage() {
+        // TODO: choisir une image pour ce plugin
+        return null;
+    }
 }
