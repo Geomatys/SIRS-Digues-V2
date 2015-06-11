f2b9e45cb53dccefd21f867e1f7446967139b710
Alexis Manin <alexis.manin@geomatys.com>
2015-06-11 11:04:08 +0200

    Change JSON serialisation rules to write WKT for geometry.
    Jira  : SYM-650

--- sirs-core/src/main/java/fr/sirs/util/json/GeometryDeserializer.java
+++ sirs-core/src/main/java/fr/sirs/util/json/GeometryDeserializer.java
@@ -9,11 +9,10 @@ import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.vividsolutions.jts.geom.Geometry;
 import com.vividsolutions.jts.io.ParseException;
-import com.vividsolutions.jts.io.WKBReader;
+import com.vividsolutions.jts.io.WKTReader;
 import fr.sirs.core.InjectorCore;
 import fr.sirs.core.SessionCore;
 
-import net.iharder.Base64;
 
 import org.geotoolkit.geometry.jts.JTS;
 
@@ -23,12 +22,11 @@ public class GeometryDeserializer extends JsonDeserializer<Geometry> {
     public Geometry deserialize(JsonParser parser, DeserializationContext arg1) throws IOException, JsonProcessingException {
         if(parser.getText()==null || parser.getText().isEmpty()) return null;
         try {
-            Geometry poly = (Geometry) new WKBReader().read(Base64.decode(parser.getText()));
-            JTS.setCRS(poly, InjectorCore.getBean(SessionCore.class).getProjection());
-            return poly;
+            Geometry value = new WKTReader().read(parser.getText());
+            JTS.setCRS(value, InjectorCore.getBean(SessionCore.class).getProjection());
+            return value;
         } catch (ParseException e) {
             throw new IOException(e);
         }
     }
-
 }
--- sirs-core/src/main/java/fr/sirs/util/json/GeometrySerializer.java
+++ sirs-core/src/main/java/fr/sirs/util/json/GeometrySerializer.java
@@ -1,24 +1,20 @@
 package fr.sirs.util.json;
 
-import java.io.IOException;
-
-import net.iharder.Base64;
-
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
+
+import java.io.IOException;
+
 import com.vividsolutions.jts.geom.Geometry;
-import com.vividsolutions.jts.io.WKBWriter;
+import com.vividsolutions.jts.io.WKTWriter;
 
 public class GeometrySerializer extends JsonSerializer<Geometry> {
 
     @Override
-    public void serialize(Geometry value,
-            com.fasterxml.jackson.core.JsonGenerator jgen,
-            SerializerProvider provider) throws IOException,
-            com.fasterxml.jackson.core.JsonProcessingException {
-        WKBWriter wkbWriter = new WKBWriter();
-        jgen.writeString(Base64.encodeBytes(wkbWriter.write(value)));
-
+    public void serialize(Geometry value, JsonGenerator jgen, SerializerProvider provider)
+            throws IOException, JsonProcessingException {
+        jgen.writeString(new WKTWriter().write(value));
     }
-
 }
