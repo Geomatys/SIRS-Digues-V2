265f831ace7ba0a046380e742966fbc19bd12d9d
Cédric Briançon <cedric.briancon@geomatys.com>
2015-07-10 14:33:12 +0200

    feat(reglementaire): add SE class column for obligations list + fix column orders

--- plugin-reglementary/src/main/java/fr/sirs/plugin/reglementaire/ui/ObligationsPojoTable.java
+++ plugin-reglementary/src/main/java/fr/sirs/plugin/reglementaire/ui/ObligationsPojoTable.java
@@ -3,22 +3,30 @@ package fr.sirs.plugin.reglementaire.ui;
 import fr.sirs.Injector;
 import fr.sirs.SIRS;
 import fr.sirs.core.component.AbstractSIRSRepository;
+import fr.sirs.core.component.SystemeEndiguementRepository;
 import fr.sirs.core.model.Element;
+import fr.sirs.core.model.LabelMapper;
 import fr.sirs.core.model.ObligationReglementaire;
 import fr.sirs.core.model.RappelObligationReglementaire;
+import fr.sirs.core.model.SystemeEndiguement;
 import fr.sirs.theme.ui.PojoTable;
 import fr.sirs.util.FXFreeTab;
 import fr.sirs.util.SimpleFXEditMode;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.control.Button;
 import javafx.scene.control.Separator;
 import javafx.scene.control.TabPane;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.Tooltip;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
+import javafx.util.Callback;
+import org.geotoolkit.gui.javafx.util.FXStringCell;
 
 /**
  * Table présentant les obligations réglementaires.
@@ -27,12 +35,15 @@ import javafx.scene.layout.Priority;
  */
 public final class ObligationsPojoTable extends PojoTable {
     /**
-     * 
+     * Création de la table présentant les obligations réglementaires.
+     *
      * @param obligationRepository Repository d'accès aux données d'obligations réglementaires
-     * @param tabPane Conteneur d'onglets dans lequel afficher l'onglet de planification
+     * @param tabPane              Conteneur d'onglets dans lequel afficher l'onglet de planification
      */
     public ObligationsPojoTable(final AbstractSIRSRepository obligationRepository, final TabPane tabPane) {
         super(obligationRepository, "Liste des obligations réglementaires");
+
+        getUiTable().getColumns().add(5, new ClassTableColumn());
 
         final Button uiPlanificationBtn = new Button(null, new ImageView(SIRS.ICON_CLOCK_WHITE));
         uiPlanificationBtn.getStyleClass().add(BUTTON_STYLE);
@@ -63,8 +74,25 @@ public final class ObligationsPojoTable extends PojoTable {
         tabPane.getSelectionModel().select(planTab);
     }
 
-    @Override
-    public ObservableList<Element> getAllValues() {
-        return super.getAllValues();
+    /**
+     * Colonne représentant la classe du système d'endiguement
+     */
+    private class ClassTableColumn extends TableColumn<ObligationReglementaire, String> {
+        public ClassTableColumn() {
+            super("Classe");
+
+            setCellValueFactory(param -> {
+                final ObligationReglementaire obligation = param.getValue();
+                if (obligation.getSystemeEndiguementId() != null) {
+                    final SystemeEndiguement se = Injector.getBean(SystemeEndiguementRepository.class).get(obligation.getSystemeEndiguementId());
+                    return new SimpleStringProperty(se.getClassement());
+                }
+                return new SimpleStringProperty();
+            });
+
+            setCellFactory(param -> new FXStringCell());
+
+            setEditable(false);
+        }
     }
 }
