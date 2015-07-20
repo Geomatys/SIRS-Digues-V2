4c0a17381287f00e5ac685ff7eb458c57210656c
Samuel Andrés <samuel.andres@geomatys.com>
2015-07-20 15:21:44 +0200

    feat(aot/cot): temporaty Convention model classes; replication of convention core importers.

---
+++ plugin-aot-cot/src/main/java/fr/sirs/importer/documentTroncon/document/convention/NewConventionImporter.java
@@ -0,0 +1,124 @@
+package fr.sirs.importer.documentTroncon.document.convention;
+
+import fr.sirs.importer.*;
+
+import com.healthmarketscience.jackcess.Database;
+import com.healthmarketscience.jackcess.Row;
+
+import static fr.sirs.core.model.ElementCreator.createAnonymValidElement;
+import fr.sirs.core.model.NewConvention;
+import fr.sirs.core.model.RefNewConvention;
+import static fr.sirs.importer.DbImporter.TableName.CONVENTION;
+import fr.sirs.importer.documentTroncon.document.GenericDocumentRelatedImporter;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import org.ektorp.CouchDbConnector;
+
+/**
+ *
+ * @author Samuel Andrés (Geomatys)
+ */
+public class NewConventionImporter extends GenericDocumentRelatedImporter<NewConvention> {
+    
+    private final TypeNewConventionImporter typeConventionImporter;
+    private final NewConventionSignataireIntervenantImporter conventionSignataireIntervenantImporter;
+    private final NewConventionSignataireOrganismeImporter conventionSignataireOrganismeImporter;
+    
+    public NewConventionImporter(final Database accessDatabase,
+            final CouchDbConnector couchDbConnector,
+            final IntervenantImporter intervenantImporter,
+            final OrganismeImporter organismeImporter) {
+        super(accessDatabase, couchDbConnector);
+        
+        this.typeConventionImporter = new TypeNewConventionImporter(accessDatabase, couchDbConnector);
+        this.conventionSignataireIntervenantImporter = new NewConventionSignataireIntervenantImporter(
+                accessDatabase, couchDbConnector, intervenantImporter);
+        this.conventionSignataireOrganismeImporter = new NewConventionSignataireOrganismeImporter(
+                accessDatabase, couchDbConnector, organismeImporter);
+    }
+
+    private enum Columns {
+        ID_CONVENTION,
+        LIBELLE_CONVENTION,
+        ID_TYPE_CONVENTION,
+        DATE_DEBUT_CONVENTION,
+        DATE_FIN_CONVENTION,
+        REFERENCE_PAPIER,
+        REFERENCE_NUMERIQUE,
+        COMMENTAIRE,
+        DATE_DERNIERE_MAJ
+    };
+    
+    
+    @Override
+    protected List<String> getUsedColumns() {
+        final List<String> columns = new ArrayList<>();
+        for (Columns c : Columns.values()) {
+            columns.add(c.toString());
+        }
+        return columns;
+    }
+
+    @Override
+    public String getTableName() {
+        return CONVENTION.toString();
+    }
+
+    @Override
+    protected void compute() throws IOException, AccessDbImporterException {
+        related = new HashMap<>();
+        
+        final Map<Integer, RefNewConvention> typesConvention = typeConventionImporter.getTypeReferences();
+        final Map<Integer, List<String>> orgSignataires = conventionSignataireOrganismeImporter.getOrganisationsSignatairesIds();
+        final Map<Integer, List<String>> intSignataires = conventionSignataireIntervenantImporter.getIntervenantsSignatairesIds();
+
+        final Iterator<Row> it = this.accessDatabase.getTable(getTableName()).iterator();
+        while (it.hasNext()) {
+            final Row row = it.next();
+            final NewConvention convention = createAnonymValidElement(NewConvention.class);
+            
+            convention.setLibelle(row.getString(Columns.LIBELLE_CONVENTION.toString()));
+            
+            convention.setTypeConventionId(typesConvention.get(row.getInt(Columns.ID_TYPE_CONVENTION.toString())).getId());
+            
+            if (row.getDate(Columns.DATE_DEBUT_CONVENTION.toString()) != null) {
+                convention.setDate_debut(DbImporter.parseLocalDate(row.getDate(Columns.DATE_DEBUT_CONVENTION.toString()), dateTimeFormatter));
+            }
+            
+            if (row.getDate(Columns.DATE_FIN_CONVENTION.toString()) != null) {
+                convention.setDate_fin(DbImporter.parseLocalDate(row.getDate(Columns.DATE_FIN_CONVENTION.toString()), dateTimeFormatter));
+            }
+            
+            convention.setReferencePapier(row.getString(Columns.REFERENCE_PAPIER.toString()));
+            
+            convention.setChemin(row.getString(Columns.REFERENCE_NUMERIQUE.toString()));
+            
+            convention.setCommentaire(row.getString(Columns.COMMENTAIRE.toString()));
+            
+            if (row.getDate(Columns.DATE_DERNIERE_MAJ.toString()) != null) {
+                convention.setDateMaj(DbImporter.parseLocalDate(row.getDate(Columns.DATE_DERNIERE_MAJ.toString()), dateTimeFormatter));
+            }
+            
+            final List<String> organismesSignatairesIds = orgSignataires.get(row.getInt(Columns.ID_CONVENTION.toString()));
+            if(organismesSignatairesIds!=null){
+                convention.setOrganismeSignataireIds(organismesSignatairesIds);
+            }
+            
+            final List<String> intervenantsSignatairesIds = intSignataires.get(row.getInt(Columns.ID_CONVENTION.toString()));
+            if(intervenantsSignatairesIds!=null){
+                convention.setContactSignataireIds(intervenantsSignatairesIds);
+            }
+
+            convention.setDesignation(String.valueOf(row.getInt(Columns.ID_CONVENTION.toString())));
+            
+            related.put(row.getInt(Columns.ID_CONVENTION.toString()), convention);
+        }
+        couchDbConnector.executeBulk(related.values());
+    }
+}
---
+++ plugin-aot-cot/src/main/java/fr/sirs/importer/documentTroncon/document/convention/NewConventionSignataireIntervenantImporter.java
@@ -0,0 +1,93 @@
+package fr.sirs.importer.documentTroncon.document.convention;
+
+import com.healthmarketscience.jackcess.Database;
+import com.healthmarketscience.jackcess.Row;
+
+import fr.sirs.core.model.Contact;
+import fr.sirs.importer.AccessDbImporterException;
+import static fr.sirs.importer.DbImporter.TableName.*;
+import fr.sirs.importer.GenericImporter;
+import fr.sirs.importer.IntervenantImporter;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import org.ektorp.CouchDbConnector;
+
+/**
+ *
+ * @author Samuel Andrés (Geomatys)
+ */
+class NewConventionSignataireIntervenantImporter extends GenericImporter {
+
+    private Map<Integer, List<String>> signatairesIdsByConventionId = null;
+    private IntervenantImporter intervenantImporter;
+
+    private NewConventionSignataireIntervenantImporter(final Database accessDatabase,
+            final CouchDbConnector couchDbConnector) {
+        super(accessDatabase, couchDbConnector);
+    }
+
+    NewConventionSignataireIntervenantImporter(final Database accessDatabase,
+            final CouchDbConnector couchDbConnector, 
+            final IntervenantImporter intervenantImporter) {
+        this(accessDatabase, couchDbConnector);
+        this.intervenantImporter = intervenantImporter;
+    }
+
+    private enum Columns {
+        ID_CONVENTION,
+        ID_INTERV_SIGNATAIRE,
+        DATE_DERNIERE_MAJ
+    };
+    
+    public Map<Integer, List<String>> getIntervenantsSignatairesIds() throws IOException, AccessDbImporterException {
+        if (signatairesIdsByConventionId == null) compute();
+        return signatairesIdsByConventionId;
+    }
+
+    @Override
+    protected List<String> getUsedColumns() {
+        final List<String> columns = new ArrayList<>();
+        for (Columns c : Columns.values()) {
+            columns.add(c.toString());
+        }
+        return columns;
+    }
+
+    @Override
+    public String getTableName() {
+        return CONVENTION_SIGNATAIRES_PP.toString();
+    }
+
+    @Override
+    protected void compute() throws IOException, AccessDbImporterException {
+        
+        signatairesIdsByConventionId = new HashMap<>();
+
+        final Map<Integer, Contact> intervenants = intervenantImporter.getIntervenants();
+        
+        final Iterator<Row> it = this.accessDatabase.getTable(getTableName()).iterator();
+        while (it.hasNext()) {
+            final Row row = it.next();
+            
+            // Set the references.
+            final Contact intervenant = intervenants.get(row.getInt(Columns.ID_INTERV_SIGNATAIRE.toString()));
+            
+            List<String> listeSignatairesIds = signatairesIdsByConventionId.get(row.getInt(Columns.ID_CONVENTION.toString()));
+            if(listeSignatairesIds == null){
+                listeSignatairesIds = new ArrayList<>();
+            }
+            if (intervenant.getId() != null) {
+                listeSignatairesIds.add(intervenant.getId());
+            } else {
+                throw new AccessDbImporterException("Le contact " + intervenant + " n'a pas encore d'identifiant CouchDb !");
+            }
+            signatairesIdsByConventionId.put(row.getInt(Columns.ID_CONVENTION.toString()), listeSignatairesIds);
+        }
+    }
+}
