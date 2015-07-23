5e10d2764124e6aa788bb09b896413039dbf014c
Alexis Manin <alexis.manin@geomatys.com>
2015-07-23 11:21:37 +0200

    refactor(core): Add error catching on application start, to avoid unexpected crash.

--- launcher/src/main/java/fr/sirs/launcher/Launcher.java
+++ launcher/src/main/java/fr/sirs/launcher/Launcher.java
@@ -8,7 +8,6 @@ import static fr.sirs.core.SirsCore.browseURL;
 import fr.sirs.core.plugins.PluginLoader;
 import fr.sirs.core.authentication.SIRSAuthenticator;
 import fr.sirs.util.SystemProxySelector;
-import java.io.IOException;
 import java.net.Authenticator;
 import java.net.ProxySelector;
 import java.util.Optional;
@@ -47,25 +46,37 @@ public class Launcher extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-    
+
     @Override
     public void start(Stage primaryStage) throws Exception {
-        primaryStage.getIcons().add(SIRS.ICON);
-        SLF4JBridgeHandler.removeHandlersForRootLogger();  // (since SLF4J 1.6.5)
-
         // add SLF4JBridgeHandler to j.u.l's root logger, should be done once during
         // the initialization phase of your application
-        SLF4JBridgeHandler.install();
-        Thread.setDefaultUncaughtExceptionHandler((Thread t, Throwable e) -> {
-            final String errorCode = UUID.randomUUID().toString();
-            SirsCore.LOGGER.log(Level.SEVERE, errorCode, e);
-            // deactivates to avoid being bothered for minor UI errors.
-//            final Runnable exceptionDialog = () -> {
-//                SIRS.newExceptionDialog("Une erreur inattendue est survenue.Code d'erreur : "+errorCode, e).show();
-//            };
-//            if (Platform.isFxApplicationThread()) exceptionDialog.run();
-//            else Platform.runLater(exceptionDialog);
-        });
+        try {
+            SLF4JBridgeHandler.removeHandlersForRootLogger();  // (since SLF4J 1.6.5)
+            SLF4JBridgeHandler.install();
+        } catch (Exception e) {
+            SirsCore.LOGGER.log(Level.SEVERE, "Cannot initialize log dumping (logbak)", e);
+            // We allow starting program without log writing.
+        }
+
+        try {
+            Thread.setDefaultUncaughtExceptionHandler((Thread t, Throwable e) -> {
+                final String errorCode = UUID.randomUUID().toString();
+                SirsCore.LOGGER.log(Level.SEVERE, errorCode, e);
+            });
+        } catch (Exception e) {
+            SirsCore.LOGGER.log(Level.SEVERE, "Cannot initialize uncaught exception management.", e);
+            // We allow starting program without that feature.
+        }
+
+        String version = null;
+        try {
+            version = SIRS.getVersion();
+        } catch (Exception e) {
+            SirsCore.LOGGER.log(Level.WARNING, "Cannot retrieve application version.", e);
+        }
+        if (version == null)
+            version = "";
 
         ProgressIndicator progressIndicator = new ProgressIndicator();
         progressIndicator.setBackground(Background.EMPTY);
@@ -78,16 +89,13 @@ public class Launcher extends Application {
         final Scene scene = new Scene(stackPane);
         scene.setFill(null);
 
-        String version = SIRS.getVersion();
-        if (version == null)
-            version = "";
-        
         final Stage splashStage = new Stage();
         splashStage.getIcons().add(SIRS.ICON);
         splashStage.setTitle("SIRS "+version);
         splashStage.initStyle(StageStyle.TRANSPARENT);
         splashStage.setScene(scene);
 
+        primaryStage.getIcons().add(SIRS.ICON);
         primaryStage.setTitle("SIRS "+version);
         primaryStage.setOnCloseRequest((WindowEvent event) -> {
             System.exit(0);
@@ -96,7 +104,7 @@ public class Launcher extends Application {
         splashStage.show();
 
         /*
-         * Initialize / create EPSG db. A loader is displayed while the task is 
+         * Initialize / create EPSG db. A loader is displayed while the task is
          * running, preventing application launch.
          */
         final Task<Boolean> initer = new Initer();
@@ -104,7 +112,7 @@ public class Launcher extends Application {
         splashLabel.textProperty().addListener((ObservableValue<? extends String> observable, String oldValue, String newValue) -> {
             splashStage.sizeToScene();
         });
-        
+
         initer.setOnSucceeded((WorkerStateEvent event) -> {
             FXLauncherPane launcherPane;
             try {
@@ -133,7 +141,7 @@ public class Launcher extends Application {
     public void stop() throws Exception {
         SirsCore.getTaskManager().close();
     }
-    
+
     /**
      * Check if there's any update available on SIRS server. If any, user is asked for download.
      */
@@ -175,7 +183,7 @@ public class Launcher extends Application {
         }
         return false;
     }
-    
+
     /**
      * Task which takes care of components initialisation, as EPSG database and plugins.
      */
@@ -186,14 +194,14 @@ public class Launcher extends Application {
             updateMessage("Analyse des configurations réseau");
             ProxySelector.setDefault(new SystemProxySelector());
             Authenticator.setDefault(new SIRSAuthenticator());
-            
+
             updateMessage("Vérification des mises à jour");
             boolean updateRequired = checkUpdate();
             if (updateRequired) {
                 cancel();
                 return false;
             }
-            
+
             updateMessage("Initialisation de la base EPSG");
             SirsCore.initEpsgDB();
 
