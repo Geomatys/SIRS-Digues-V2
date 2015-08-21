917534f55d1aa431778469290c318777bf806a67
Samuel Andrés <samuel.andres@geomatys.com>
2015-08-21 11:21:42 +0200

    chore(vegetation): delete surface field from ZoneVegetation subclasses. Add libelle to ParcelleVegetation.

---
+++ plugin-vegetation/src/main/java/fr/sirs/theme/ui/FXParcelleVegetationPane.java
@@ -0,0 +1,17 @@
+
+package fr.sirs.theme.ui;
+
+import fr.sirs.core.model.ParcelleVegetation;
+
+/**
+ *
+ * @author Samuel Andrés (Geomatys)
+ */
+public class FXParcelleVegetationPane extends FXParcelleVegetationPaneStub {
+    
+    public FXParcelleVegetationPane(final ParcelleVegetation parcelleVegetation){
+        super(parcelleVegetation);
+    }
+
+    
+}
