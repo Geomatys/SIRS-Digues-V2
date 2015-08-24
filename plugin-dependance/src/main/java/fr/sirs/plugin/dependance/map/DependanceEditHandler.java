bcacd585d916993f6adf4febeaf16d1c6289cb96
Cédric Briançon <cedric.briancon@geomatys.com>
2015-08-24 16:41:13 +0200

    feat(dependance): add popup for right click actions on dependance

--- plugin-dependance/src/main/java/fr/sirs/plugin/dependance/map/DependanceEditHandler.java
+++ plugin-dependance/src/main/java/fr/sirs/plugin/dependance/map/DependanceEditHandler.java
@@ -2,54 +2,54 @@ package fr.sirs.plugin.dependance.map;
 
 import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.Geometry;
-import com.vividsolutions.jts.geom.GeometryFactory;
-import com.vividsolutions.jts.geom.LinearRing;
 import com.vividsolutions.jts.geom.Polygon;
 import fr.sirs.Injector;
-import fr.sirs.SIRS;
 import fr.sirs.core.component.AbstractSIRSRepository;
 import fr.sirs.core.component.AireStockageDependanceRepository;
+import fr.sirs.core.component.AutreDependanceRepository;
+import fr.sirs.core.component.CheminAccesDependanceRepository;
+import fr.sirs.core.component.OuvrageVoirieDependanceRepository;
 import fr.sirs.core.model.AbstractDependance;
 import fr.sirs.core.model.AireStockageDependance;
 import fr.sirs.core.model.AutreDependance;
 import fr.sirs.core.model.CheminAccesDependance;
 import fr.sirs.core.model.OuvrageVoirieDependance;
 import fr.sirs.plugin.dependance.PluginDependance;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.geometry.Side;
 import javafx.scene.Cursor;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.ChoiceDialog;
-import javafx.scene.control.ComboBox;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.MenuItem;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.input.ScrollEvent;
-import org.apache.sis.internal.referencing.j2d.AffineTransform2D;
-import org.apache.sis.storage.DataStoreException;
-import org.geotoolkit.data.FeatureCollection;
 import org.geotoolkit.data.bean.BeanFeature;
-import org.geotoolkit.data.query.QueryBuilder;
-import org.geotoolkit.factory.FactoryFinder;
-import org.geotoolkit.factory.Hints;
-import org.geotoolkit.feature.Feature;
+import org.geotoolkit.display.VisitFilter;
+import org.geotoolkit.display2d.canvas.AbstractGraphicVisitor;
+import org.geotoolkit.display2d.canvas.RenderingContext2D;
+import org.geotoolkit.display2d.primitive.ProjectedCoverage;
+import org.geotoolkit.display2d.primitive.ProjectedFeature;
+import org.geotoolkit.display2d.primitive.SearchAreaJ2D;
 import org.geotoolkit.geometry.jts.JTS;
 import org.geotoolkit.gui.javafx.render2d.AbstractNavigationHandler;
 import org.geotoolkit.gui.javafx.render2d.FXMap;
 import org.geotoolkit.gui.javafx.render2d.FXPanMouseListen;
 import org.geotoolkit.gui.javafx.render2d.edition.EditionHelper;
 import org.geotoolkit.gui.javafx.render2d.shape.FXGeometryLayer;
+import org.geotoolkit.internal.GeotkFX;
 import org.geotoolkit.map.FeatureMapLayer;
-import org.opengis.filter.Filter;
-import org.opengis.filter.FilterFactory2;
-import org.opengis.filter.expression.Expression;
-
-import java.awt.geom.AffineTransform;
-import java.awt.geom.NoninvertibleTransformException;
+import java.awt.geom.Rectangle2D;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.logging.Level;
+
+import static javafx.scene.control.Alert.AlertType.CONFIRMATION;
+import static javafx.scene.control.ButtonType.NO;
+import static javafx.scene.control.ButtonType.YES;
 
 /**
  * Contrôle les actions possibles pour le bouton d'édition et de modification de dépendances
@@ -59,9 +59,6 @@ import java.util.logging.Level;
  * @author Johann Sorel (Geomatys)
  */
 public class DependanceEditHandler extends AbstractNavigationHandler {
-    private static final FilterFactory2 FF = (FilterFactory2) FactoryFinder.getFilterFactory(
-            new Hints(Hints.FILTER_FACTORY, FilterFactory2.class));
-
     private final MouseListen mouseInputListener = new MouseListen();
     private final FXGeometryLayer decorationLayer = new FXGeometryLayer();
 
@@ -148,71 +145,11 @@ public class DependanceEditHandler extends AbstractNavigationHandler {
      * Ecoute les évènements lancés par la souris.
      */
     private class MouseListen extends FXPanMouseListen {
-        private final GeometryFactory GEOMETRY_FACTORY = new GeometryFactory();
+        private final ContextMenu popup = new ContextMenu();
         private MouseButton pressed;
 
         public MouseListen() {
             super(DependanceEditHandler.this);
-        }
-
-        /**
-         * Construit un filtre d'intersection entre une géométrie polygone et une couche de la carte.
-         *
-         * @param poly Le polygone choisi pour l'intersection
-         * @param fl La couche de données de la carte.
-         * @return Le filtre d'intersection.
-         */
-        private Filter toFilter(final Geometry poly, final FeatureMapLayer fl) {
-            final String geoStr = fl.getCollection().getFeatureType().getGeometryDescriptor().getLocalName();
-            final Expression geomField = FF.property(geoStr);
-
-            final Geometry dataPoly = poly;
-            JTS.setCRS(dataPoly, map.getCanvas().getObjectiveCRS2D());
-
-            final Expression geomData = FF.literal(dataPoly);
-            return FF.intersects(geomData,geomField);
-        }
-
-        /**
-         * Transforme les coordonées fournies en géométrie exprimée dans le CRS de la carte.
-         *
-         * @param x Coordonnée x de la souris
-         * @param y Coordonnée y de la souris
-         * @return Les coordonnées de ce point.
-         */
-        public Coordinate toCoord(final double x, final double y) {
-            final AffineTransform2D trs = map.getCanvas().getObjectiveToDisplay();
-            AffineTransform dispToObj;
-            try {
-                dispToObj = trs.createInverse();
-            } catch (NoninvertibleTransformException ex) {
-                dispToObj = new AffineTransform();
-                SIRS.LOGGER.log(Level.WARNING, null, ex);
-            }
-            final double[] crds = new double[]{x, y};
-            dispToObj.transform(crds, 0, crds, 0, 1);
-
-            return new Coordinate(crds[0], crds[1]);
-        }
-
-        /**
-         * Transforme un point cliqué à la souris en un polygone.
-         *
-         * @param mx : Coordonnée x de la souris sur la carte (en pixel)
-         * @param my : Coordonnée y de la souris sur la carte (en pixel)
-         * @return une géométrie JTS (correspondant à un carré de 6x6 pixels autour des coordonnées de la souris)
-         */
-        private Polygon mousePositionToGeometry(final double mx, final double my) throws NoninvertibleTransformException {
-            int mousePointerSize = 4;
-            final Coordinate[] coord = new Coordinate[5];
-            coord[0] = toCoord(mx - mousePointerSize, my - mousePointerSize);
-            coord[1] = toCoord(mx - mousePointerSize, my + mousePointerSize);
-            coord[2] = toCoord(mx + mousePointerSize, my + mousePointerSize);
-            coord[3] = toCoord(mx + mousePointerSize, my - mousePointerSize);
-            coord[4] = coord[0];
-
-            final LinearRing lr1 = GEOMETRY_FACTORY.createLinearRing(coord);
-            return GEOMETRY_FACTORY.createPolygon(lr1, null);
         }
 
         @Override
@@ -223,50 +160,30 @@ public class DependanceEditHandler extends AbstractNavigationHandler {
             if (MouseButton.PRIMARY.equals(e.getButton())) {
                 if (dependance == null) {
                     // Recherche d'une couche de la carte qui contiendrait une géométrie là où l'utilisateur a cliqué
-                    try {
-                        final Polygon polygonClicked = mousePositionToGeometry(x, y);
-                        final FeatureCollection aireCol = aireLayer.getCollection().subCollection(
-                                QueryBuilder.filtered(aireLayer.getCollection().getFeatureType().getName(),
-                                toFilter(polygonClicked, aireLayer)));
-                        if (!aireCol.isEmpty()) {
-                            helper = new EditionHelper(map, aireLayer);
-                        } else {
-                            final FeatureCollection autreCol = autreLayer.getCollection().subCollection(
-                                    QueryBuilder.filtered(autreLayer.getCollection().getFeatureType().getName(),
-                                    toFilter(polygonClicked, autreLayer)));
-                            if (!autreCol.isEmpty()) {
-                                helper = new EditionHelper(map, autreLayer);
-                            } else {
-                                final FeatureCollection cheminCol = cheminLayer.getCollection().subCollection(
-                                        QueryBuilder.filtered(cheminLayer.getCollection().getFeatureType().getName(),
-                                        toFilter(polygonClicked, cheminLayer)));
-                                if (!cheminCol.isEmpty()) {
-                                    helper = new EditionHelper(map, cheminLayer);
-                                } else {
-                                    helper = new EditionHelper(map, ouvrageLayer);
-                                }
+                    final Rectangle2D clickArea = new Rectangle2D.Double(e.getX()-2, e.getY()-2, 4, 4);
+
+                    //recherche d'un object a editer
+                    map.getCanvas().getGraphicsIn(clickArea, new AbstractGraphicVisitor() {
+                        @Override
+                        public void visit(ProjectedFeature graphic, RenderingContext2D context, SearchAreaJ2D area) {
+                            final Object bean = graphic.getCandidate().getUserData().get(BeanFeature.KEY_BEAN);
+                            if(bean instanceof AbstractDependance){
+                                helper = new EditionHelper(map, graphic.getLayer());
+                                dependance = (AbstractDependance)bean;
+                                // On récupère la géométrie de cet objet pour passer en mode édition
+                                editGeometry.geometry.set(dependance.getGeometry());
+                                // Ajout de cette géométrie dans la couche d'édition sur la carte.
+                                decorationLayer.getGeometries().add(editGeometry.geometry.get());
+                                newDependance = false;
                             }
                         }
-                    } catch (NoninvertibleTransformException | DataStoreException ex) {
-                        SIRS.LOGGER.log(Level.INFO, ex.getLocalizedMessage(), ex);
-                        return;
-                    }
-
-                    // La dépendance n'est pas sélectionnée, on essaye de la trouver par rapport aux coordonnées
-                    // du clic de la souris.
-                    final Feature feature = helper.grabFeature(x, y, true);
-                    if (feature != null) {
-                        // Il y a une feature là où l'utilisateur a cliqué, vérification du type
-                        final Object candidate = feature.getUserData().get(BeanFeature.KEY_BEAN);
-                        if (candidate instanceof AbstractDependance) {
-                            dependance = (AbstractDependance)candidate;
-                            // On récupère la géométrie de cet objet pour passer en mode édition
-                            editGeometry.geometry.set(dependance.getGeometry());
-                            // Ajout de cette géométrie dans la couche d'édition sur la carte.
-                            decorationLayer.getGeometries().add(editGeometry.geometry.get());
-                            newDependance = false;
+                        @Override
+                        public boolean isStopRequested() {
+                            return helper!=null;
                         }
-                    }
+                        @Override
+                        public void visit(ProjectedCoverage coverage, RenderingContext2D context, SearchAreaJ2D area) {}
+                    }, VisitFilter.INTERSECTS);
                 } else {
                     // La dépendance existe, on peut travailler avec sa géométrie.
                     if (newDependance) {
@@ -339,17 +256,71 @@ public class DependanceEditHandler extends AbstractNavigationHandler {
                         newDependance = true;
                     });
                 } else {
-                    // Sauvegarde de la dépendance de stockage ainsi que sa géométrie qui a éventuellement été éditée.
-                    dependance.setGeometry(editGeometry.geometry.get());
-                    final AbstractSIRSRepository repodep = Injector.getSession().getRepositoryForClass(dependance.getClass());
+                    // popup :
+                    // -suppression d'un noeud
+                    // -sauvegarder
+                    // -annuler édition
+                    // -supprimer dépendance
+                    popup.getItems().clear();
 
-                    if (dependance.getDocumentId() != null) {
-                        repodep.update(dependance);
-                    } else {
-                        repodep.add(dependance);
+                    //action : suppression d'un noeud
+                    helper.grabGeometryNode(e.getX(), e.getY(), editGeometry);
+                    if (editGeometry.selectedNode[0] >= 0) {
+                        final MenuItem item = new MenuItem("Supprimer noeud");
+                        item.setOnAction((ActionEvent event) -> {
+                            editGeometry.deleteSelectedNode();
+                            decorationLayer.setNodeSelection(null);
+                            decorationLayer.getGeometries().setAll(editGeometry.geometry.get());
+                        });
+                        popup.getItems().add(item);
                     }
-                    // On quitte le mode d'édition.
-                    reset();
+
+                    // action : sauvegarde
+                    // Sauvegarde de la dépendance de stockage ainsi que sa géométrie qui a éventuellement été éditée.
+                    final MenuItem saveItem = new MenuItem("Sauvegarder");
+                    saveItem.setOnAction((ActionEvent event) -> {
+                        dependance.setGeometry(editGeometry.geometry.get());
+                        final AbstractSIRSRepository repodep = Injector.getSession().getRepositoryForClass(dependance.getClass());
+
+                        if (dependance.getDocumentId() != null) {
+                            repodep.update(dependance);
+                        } else {
+                            repodep.add(dependance);
+                        }
+                        // On quitte le mode d'édition.
+                        reset();
+                    });
+                    popup.getItems().add(saveItem);
+
+                    // action : annuler édition
+                    final MenuItem cancelItem = new MenuItem("Annuler l'édition");
+                    cancelItem.setOnAction(event -> {
+                        reset();
+                    });
+                    popup.getItems().add(cancelItem);
+
+                    // action : suppression dépendance
+                    final MenuItem deleteItem = new MenuItem("Supprimer dépendance", new ImageView(GeotkFX.ICON_DELETE));
+                    deleteItem.setOnAction((ActionEvent event) -> {
+                        final Alert alert = new Alert(CONFIRMATION, "Voulez-vous vraiment supprimer la dépendance sélectionnée ?", YES, NO);
+                        final Optional<ButtonType> result = alert.showAndWait();
+                        if (result.isPresent() && result.get()==YES) {
+                            if (AireStockageDependance.class.isAssignableFrom(dependance.getClass())) {
+                                Injector.getBean(AireStockageDependanceRepository.class).remove((AireStockageDependance)dependance);
+                            } else if (AutreDependance.class.isAssignableFrom(dependance.getClass())) {
+                                Injector.getBean(AutreDependanceRepository.class).remove((AutreDependance)dependance);
+                            } else if (CheminAccesDependance.class.isAssignableFrom(dependance.getClass())) {
+                                Injector.getBean(CheminAccesDependanceRepository.class).remove((CheminAccesDependance)dependance);
+                            } else if (OuvrageVoirieDependance.class.isAssignableFrom(dependance.getClass())) {
+                                Injector.getBean(OuvrageVoirieDependanceRepository.class).remove((OuvrageVoirieDependance)dependance);
+                            }
+                            // On quitte le mode d'édition.
+                            reset();
+                        }
+                    });
+                    popup.getItems().add(deleteItem);
+                    
+                    popup.show(decorationLayer, Side.TOP, e.getX(), e.getY());
                 }
             }
         }
@@ -392,5 +363,6 @@ public class DependanceEditHandler extends AbstractNavigationHandler {
         coords.clear();
         editGeometry.reset();
         dependance = null;
+        helper = null;
     }
 }
