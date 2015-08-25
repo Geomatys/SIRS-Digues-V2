732517068ad5ce39edec29cfb31dfcc05e2fb42e
Johann Sorel <johann.sorel@geomatys.com>
2015-08-25 17:51:06 +0200

    feat(Vegetation): interface d'impression

---
+++ plugin-vegetation/src/main/java/fr/sirs/plugin/vegetation/FXImpression.java
@@ -0,0 +1,54 @@
+
+package fr.sirs.plugin.vegetation;
+
+import fr.sirs.SIRS;
+import fr.sirs.core.model.Positionable;
+import fr.sirs.core.model.Preview;
+import java.time.LocalDate;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
+import javafx.scene.layout.GridPane;
+
+/**
+ *
+ * @author Johann Sorel (Geomatys)
+ */
+public class FXImpression extends GridPane{
+
+    @FXML private GridPane uiGrid;
+    @FXML private ListView<Preview> uiTroncons;
+    @FXML private ComboBox<LocalDate> uiDateStart;
+    @FXML private ComboBox<LocalDate> uiDateEnd;
+    @FXML private CheckBox uiAllTroncon;
+    
+    @FXML private CheckBox uiTraiteeNonPlanif;
+    @FXML private CheckBox uiTraiteePlanif;
+    @FXML private CheckBox uiNonTraiteeNonPlanif;
+    @FXML private CheckBox uiNonTraiteePlanif;
+
+    private final Spinner<Double> uiPRStart = new Spinner<>(new SpinnerValueFactory.DoubleSpinnerValueFactory(-Double.MIN_VALUE, Double.MAX_VALUE, 0));
+    private final Spinner<Double> uiPREnd = new Spinner<>(new SpinnerValueFactory.DoubleSpinnerValueFactory(-Double.MIN_VALUE, Double.MAX_VALUE, 0));
+
+    public FXImpression() {
+        SIRS.loadFXML(this, Positionable.class);
+
+        uiGrid.add(uiPRStart, 1, 3);
+        uiGrid.add(uiPRStart, 3, 3);
+
+        //on liste les troncons du plan actif.
+
+    }
+
+    @FXML
+    void print(ActionEvent event) {
+
+        
+
+    }
+
+}
