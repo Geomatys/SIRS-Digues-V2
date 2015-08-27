261e7f59323da34c553e6e8dbaf3f38e929f18ad
Samuel Andrés <samuel.andres@geomatys.com>
2015-08-27 10:39:38 +0200

    refactor(*): move FXListTableCell to Desktop

--- desktop/src/main/java/org/geotoolkit/gui/javafx/util/FXListTableCell.java
+++ desktop/src/main/java/org/geotoolkit/gui/javafx/util/FXListTableCell.java
@@ -1,3 +1,19 @@
+/*
+ *    Geotoolkit - An Open Source Java GIS Toolkit
+ *    http://www.geotoolkit.org
+ *
+ *    (C) 2014, Geomatys
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
 package org.geotoolkit.gui.javafx.util;
 
 import java.util.List;
@@ -13,6 +29,10 @@ import javafx.util.StringConverter;
 /**
  *
  * @author Samuel Andrés (Geomatys)
+ * 
+ * @param <S> The type of the TableView generic type (i.e. S == TableView&lt;S&gt;).
+ *           This should also match with the first generic type in TableColumn.
+ * @param <T> The type of the item contained within the Cell.
  */
 public class FXListTableCell<S, T> extends TableCell<S, T> {
 
