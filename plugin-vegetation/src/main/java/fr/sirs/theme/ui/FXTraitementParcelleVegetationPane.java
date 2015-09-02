9400f35800059463aa8e2f9a88eaa96d9c26c685
Samuel Andrés <samuel.andres@geomatys.com>
2015-09-02 18:03:42 +0200

    feat(vegetation): override FXTraitementParcelleVegetationPane to bind traitement fields (empty overrided behavior for now).

---
+++ plugin-vegetation/src/main/java/fr/sirs/theme/ui/FXTraitementParcelleVegetationPane.java
@@ -0,0 +1,15 @@
+
+package fr.sirs.theme.ui;
+
+import fr.sirs.core.model.TraitementParcelleVegetation;
+
+/**
+ *
+ * @author Samuel Andrés (Geomatys)
+ */
+public class FXTraitementParcelleVegetationPane extends FXTraitementParcelleVegetationPaneStub {
+    
+    public FXTraitementParcelleVegetationPane(final TraitementParcelleVegetation traitementParcelleVegetation){
+        super(traitementParcelleVegetation);
+    }     
+}
