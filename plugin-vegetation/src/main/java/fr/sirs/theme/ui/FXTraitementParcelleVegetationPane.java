ad3cd69686e33fcc9720e9324013726932fe7aa9
Samuel Andrés <samuel.andres@geomatys.com>
2015-09-03 10:00:48 +0200

    feat(vegetation): synchonise traitement comboboxes of FXTraitementParcelleVegetationPane.

--- plugin-vegetation/src/main/java/fr/sirs/theme/ui/FXTraitementParcelleVegetationPane.java
+++ plugin-vegetation/src/main/java/fr/sirs/theme/ui/FXTraitementParcelleVegetationPane.java
@@ -1,7 +1,19 @@
 
 package fr.sirs.theme.ui;
 
+import fr.sirs.Injector;
+import fr.sirs.SIRS;
+import fr.sirs.core.component.AbstractSIRSRepository;
+import fr.sirs.core.model.Preview;
+import fr.sirs.core.model.RefSousTraitementVegetation;
 import fr.sirs.core.model.TraitementParcelleVegetation;
+import static fr.sirs.plugin.vegetation.PluginVegetation.initComboSousTraitement;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
 
 /**
  *
@@ -11,5 +23,38 @@ public class FXTraitementParcelleVegetationPane extends FXTraitementParcelleVege
     
     public FXTraitementParcelleVegetationPane(final TraitementParcelleVegetation traitementParcelleVegetation){
         super(traitementParcelleVegetation);
-    }     
+
+        ui_traitementId.getSelectionModel().selectedItemProperty().addListener(new ChangeListener() {
+
+            @Override
+            public void changed(ObservableValue observable, Object oldValue, Object newValue) {
+                if(newValue instanceof Preview){
+                    final AbstractSIRSRepository<RefSousTraitementVegetation> sousTypeRepo = Injector.getSession().getRepositoryForClass(RefSousTraitementVegetation.class);
+                    final String traitementId = ((Preview) newValue).getElementId();
+                    if(traitementId!=null){
+                        final List<RefSousTraitementVegetation> sousTypesDispos = sousTypeRepo.getAll();
+                        sousTypesDispos.removeIf((RefSousTraitementVegetation st) -> !traitementId.equals(st.getTraitementId()));
+                        SIRS.initCombo(ui_sousTraitementId, FXCollections.observableList(sousTypesDispos), null);
+                    }
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void initFields(ObservableValue<? extends TraitementParcelleVegetation > observableElement, TraitementParcelleVegetation oldElement, TraitementParcelleVegetation newElement) {
+        super.initFields(observableElement, oldElement, newElement);
+        
+        // Initialisation des sous-types
+        final AbstractSIRSRepository<RefSousTraitementVegetation> repoSousTraitements = Injector.getSession().getRepositoryForClass(RefSousTraitementVegetation.class);
+        final Map<String, RefSousTraitementVegetation> sousTraitements = new HashMap<>();
+
+        for(final RefSousTraitementVegetation sousTraitement : repoSousTraitements.getAll()){
+            sousTraitements.put(sousTraitement.getId(), sousTraitement);
+        }
+
+        final List<Preview> sousTraitementPreviews = previewRepository.getByClass(RefSousTraitementVegetation.class);
+
+        initComboSousTraitement(newElement.getTraitementId(), newElement.getSousTraitementId(), sousTraitementPreviews, sousTraitements, ui_sousTraitementId);
+    }
 }
