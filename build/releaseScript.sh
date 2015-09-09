06d27cdcc04c67d0f62eca32c94d37c4a2e73e02
Cédric Briançon <cedric.briancon@geomatys.com>
2015-09-09 10:24:57 +0200

    Prepare release script for next build.

--- build/releaseScript.sh
+++ build/releaseScript.sh
@@ -21,7 +21,7 @@
 
 ############## DEFAULT VALUES TO USE FOR RELEASE VERSION 								 ##############
 ############## WARNING : DO NOT AFFECT THOSE VARIABLES IN ANOTHER PLACE OF THIS SCRIPT ! ##############
-defaultMinor=14
+defaultMinor=15
 defaultMajor=0
 
 
