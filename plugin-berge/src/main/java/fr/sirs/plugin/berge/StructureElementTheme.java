33a02450d6e3fe2d31bc4f6b5131ea30e8ab8cfe
Samuel Andrés <samuel.andres@geomatys.com>
2015-09-21 14:47:38 +0200

    fix(berge): layout and model updates.

--- plugin-berge/src/main/java/fr/sirs/plugin/berge/StructureElementTheme.java
+++ plugin-berge/src/main/java/fr/sirs/plugin/berge/StructureElementTheme.java
@@ -4,8 +4,8 @@ package fr.sirs.plugin.berge;
 import fr.sirs.core.model.EpiBerge;
 import fr.sirs.core.model.OuvrageRevancheBerge;
 import fr.sirs.core.model.PiedBerge;
-import fr.sirs.core.model.SommetRisbermeBerge;
-import fr.sirs.core.model.TalusDigueBerge;
+import fr.sirs.core.model.SommetBerge;
+import fr.sirs.core.model.TalusBerge;
 import fr.sirs.plugin.berge.ui.AbstractDescriptionPane;
 import fr.sirs.plugin.berge.util.TabContent;
 import java.util.ArrayList;
@@ -27,8 +27,8 @@ public class StructureElementTheme extends AbstractDescriptionTheme {
     @Override
     public Parent createPane() {
         List<TabContent> content = new ArrayList<>();
-        content.add(new TabContent("Sommet", "Tableau des sommets", SommetRisbermeBerge.class));
-        content.add(new TabContent("Talus", "Tableau des talus", TalusDigueBerge.class));
+        content.add(new TabContent("Sommet", "Tableau des sommets", SommetBerge.class));
+        content.add(new TabContent("Talus", "Tableau des talus", TalusBerge.class));
         content.add(new TabContent("Pieds de berge", "Tableau des pieds de berge", PiedBerge.class));
         content.add(new TabContent("Epis", "Tableau des épis", EpiBerge.class));
         content.add(new TabContent("Ouvrage de revanche", "Tableau des ouvrage de revanche", OuvrageRevancheBerge.class));
