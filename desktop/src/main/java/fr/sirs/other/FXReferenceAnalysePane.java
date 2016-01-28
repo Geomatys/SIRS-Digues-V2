11737c357180fe4e122b7efe5c1d7c5cabf5abcf
Alexis Manin <alexis.manin@geomatys.com>
2016-01-28 12:49:57 +0100

    refactor(desktop): Remodel reference analysis panel. Avoid timeout on view request.

--- desktop/src/main/java/fr/sirs/other/FXReferenceAnalysePane.java
+++ desktop/src/main/java/fr/sirs/other/FXReferenceAnalysePane.java
@@ -1,137 +1,147 @@
-
 package fr.sirs.other;
 
 import fr.sirs.Injector;
 import fr.sirs.SIRS;
-import static fr.sirs.SIRS.BUNDLE_KEY_CLASS;
-import fr.sirs.Session;
 import fr.sirs.core.component.ReferenceUsageRepository;
-import fr.sirs.core.model.AvecLibelle;
+import fr.sirs.core.model.LabelMapper;
 import fr.sirs.core.model.ReferenceType;
 import fr.sirs.core.model.ReferenceUsage;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.io.InterruptedIOException;
 import java.util.List;
-import java.util.Locale;
-import java.util.Map;
-import java.util.ResourceBundle;
+import java.util.concurrent.TimeoutException;
+import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.logging.Level;
 import javafx.beans.property.SimpleObjectProperty;
-import javafx.beans.value.ObservableValue;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.concurrent.Task;
-import javafx.concurrent.WorkerStateEvent;
-import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.Tooltip;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
-import javafx.util.Callback;
+import org.ektorp.DbAccessException;
+import org.geotoolkit.gui.javafx.util.ButtonTableCell;
+import org.geotoolkit.gui.javafx.util.TaskManager;
 
 /**
  *
  * @author Samuel Andrés (Geomatys)
+ * @author Aleis Manin   (Geomatys)
  */
 public class FXReferenceAnalysePane extends BorderPane {
 
-    private TableView<ReferenceUsage> usages;
-    private final Session session = Injector.getSession();
-    private final Map<String, ResourceBundle> bundles = new HashMap<>();
-
     public FXReferenceAnalysePane(final ReferenceType reference) {
-        final ResourceBundle bundle = ResourceBundle.getBundle(ReferenceUsage.class.getName(), Locale.getDefault(),
-                Thread.currentThread().getContextClassLoader());
+        final LabelMapper mapper = LabelMapper.get(reference.getClass());
+        final Label uiTitle = new Label(new StringBuilder("Usages dans la base de la référence \"").append(reference.getLibelle()).append("\" (").append(mapper.mapClassName()).append(")").toString());
+        uiTitle.getStyleClass().add("pojotable-header");
+        uiTitle.setAlignment(Pos.CENTER);
+        uiTitle.setPadding(new Insets(5));
+        uiTitle.setPrefWidth(USE_COMPUTED_SIZE);
+        setTop(uiTitle);
 
-        try {
-            final Method getId = reference.getClass().getMethod("getId");
-            final String id = (String) getId.invoke(reference);
-            final ReferenceUsageRepository referenceUsageRepository = session.getReferenceUsageRepository();
-            final List<ReferenceUsage> referenceUsages = new ArrayList<>();
+        // Compute statistics for queried reference.
+        final Task<List<ReferenceUsage>> task = new TaskManager.MockTask(
+                "Recherche de l'utilisation d'une référence.",
+                () -> getUsage(reference));
+        task.setOnSucceeded(evt -> SIRS.fxRun(false, () -> displayReferences(task.getValue())));
+        Injector.getSession().getTaskManager().submit(task);
+    }
 
-            final Task task = new Task() {
+    /**
+     * Display given reference usages in a table view.
+     * @param referenceUsages Objects to display.
+     */
+    private void displayReferences(final List<ReferenceUsage> referenceUsages) {
+        if (referenceUsages.isEmpty()) {
+            setCenter(null);
+        } else {
+            final LabelMapper mapper = LabelMapper.get(ReferenceUsage.class);
 
-                @Override
-                protected Object call() throws Exception {
-                    updateMessage("Recherche de l'utilisation d'une référence.");
-                    referenceUsages.addAll(referenceUsageRepository.getReferenceUsages(id));
-                    return null;
-                }
-            };
+            final TableView<ReferenceUsage> usages = new TableView<>(SIRS.observableList(referenceUsages));
+            usages.setEditable(false);
 
-            task.setOnSucceeded(new EventHandler<WorkerStateEvent>() {
-
-                @Override
-                public void handle(WorkerStateEvent event) {
-                    if(!referenceUsages.isEmpty()){
-                        usages = new TableView<>(SIRS.observableList(referenceUsages));
-                        usages.setEditable(false);
-
-                        final TableColumn<ReferenceUsage, String> propertyColumn = new TableColumn<>(bundle.getString("property"));
-                        propertyColumn.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<ReferenceUsage, String>, ObservableValue<String>>() {
-
-                            @Override
-                            public ObservableValue<String> call(TableColumn.CellDataFeatures<ReferenceUsage, String> param) {
-                                return new SimpleObjectProperty<>(param.getValue().getProperty());
-                            }
-                        });
-                        usages.getColumns().add(propertyColumn);
-
-                        final TableColumn<ReferenceUsage, String> objectIdColumn = new TableColumn<>(bundle.getString("objectId"));
-                        objectIdColumn.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<ReferenceUsage, String>, ObservableValue<String>>() {
-
-                            @Override
-                            public ObservableValue<String> call(TableColumn.CellDataFeatures<ReferenceUsage, String> param) {
-                                return new SimpleObjectProperty(param.getValue().getObjectId());
-                            }
-                        });
-                        usages.getColumns().add(objectIdColumn);
-
-                        final TableColumn<ReferenceUsage, String> typeColumn = new TableColumn<>(bundle.getString("type"));
-                        typeColumn.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<ReferenceUsage, String>, ObservableValue<String>>() {
-
-                            @Override
-                            public ObservableValue<String> call(TableColumn.CellDataFeatures<ReferenceUsage, String> param) {
-                                String type = param.getValue().getType();
-                                if(bundles.get(type)==null) bundles.put(type, ResourceBundle.getBundle(type, Locale.getDefault(), Thread.currentThread().getContextClassLoader()));
-                                type = bundles.get(type).getString(BUNDLE_KEY_CLASS);
-                                return new SimpleObjectProperty(type);
-                            }
-                        });
-                        usages.getColumns().add(typeColumn);
-
-                        final TableColumn<ReferenceUsage, String> labelColumn = new TableColumn<>(bundle.getString("label"));
-                        labelColumn.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<ReferenceUsage, String>, ObservableValue<String>>() {
-
-                            @Override
-                            public ObservableValue<String> call(TableColumn.CellDataFeatures<ReferenceUsage, String> param) {
-                                return new SimpleObjectProperty(param.getValue().getLabel());
-                            }
-                        });
-                        usages.getColumns().add(labelColumn);
-                        setCenter(usages);
-
-                        final ResourceBundle topBundle = ResourceBundle.getBundle(reference.getClass().getName(), Locale.getDefault(), Thread.currentThread().getContextClassLoader());
-                        final Label uiTitle = new Label("Usages dans la base de la référence \""+ ((AvecLibelle)reference).getLibelle()+"\" ("+topBundle.getString(BUNDLE_KEY_CLASS)+")");
-                        uiTitle.getStyleClass().add("pojotable-header");
-                        uiTitle.setAlignment(Pos.CENTER);
-                        uiTitle.setPadding(new Insets(5));
-                        uiTitle.setPrefWidth(USE_COMPUTED_SIZE);
-                        setTop(uiTitle);
-
+            final TableColumn<ReferenceUsage, String> typeColumn = new TableColumn<>(mapper.mapPropertyName("type"));
+            typeColumn.setCellValueFactory((TableColumn.CellDataFeatures<ReferenceUsage, String> param) -> {
+                String type = param.getValue().getType();
+                try {
+                    LabelMapper targetMapper = LabelMapper.get(Class.forName(type, false, Thread.currentThread().getContextClassLoader()));
+                    if (targetMapper != null) {
+                        type = targetMapper.mapClassName();
                     }
+                } catch (ClassNotFoundException e) {
+                    SIRS.LOGGER.log(Level.FINE, "Cannot find title for type ".concat(type), e);
                 }
+
+                return new SimpleObjectProperty(type);
             });
 
-            Injector.getSession().getTaskManager().submit(task);
+            final TableColumn<ReferenceUsage, String> labelColumn = new TableColumn<>(mapper.mapPropertyName("label"));
+            labelColumn.setCellValueFactory((TableColumn.CellDataFeatures<ReferenceUsage, String> param) -> new SimpleStringProperty(param.getValue().getLabel()));
 
-        } catch (NoSuchMethodException | SecurityException | IllegalAccessException | IllegalArgumentException | InvocationTargetException ex) {
-            SIRS.LOGGER.log(Level.SEVERE, null, ex);
+            usages.getColumns().addAll(new EditColumn(), typeColumn, labelColumn);
+            setCenter(usages);
         }
     }
 
+    /**
+     * Find the {@link Element} pointed by input {@link ReferenceUsage#getObjectId()
+     * } property, and display an editor for it when user click on the cell.
+     */
+    public static class EditColumn extends TableColumn<ReferenceUsage, String> {
 
+        public EditColumn() {
+            super("Edition");
+            setSortable(false);
+            setResizable(false);
+            setPrefWidth(24);
+            setMinWidth(24);
+            setMaxWidth(24);
+            setGraphic(new ImageView(SIRS.ICON_EDIT_BLACK));
+
+            final Tooltip tooltip = new Tooltip("Ouvrir la fiche de l'élément");
+
+            final Predicate<String> enabled = param -> param != null && !param.isEmpty();
+            final Function<String, Boolean> openEditor = param -> {
+                Injector.getSession().showEditionTab(param);
+                return true;
+            };
+
+            setCellValueFactory((param) -> new SimpleStringProperty(param.getValue().getObjectId()));
+
+            setCellFactory((TableColumn<ReferenceUsage, String> param) -> {
+                final ButtonTableCell button = new ButtonTableCell(false, new ImageView(SIRS.ICON_EDIT_BLACK), o -> o != null, openEditor);
+                button.setTooltip(tooltip);
+                return button;
+            });
+        }
+    }
+
+    /**
+     * Find usages of a given reference in database.
+     *
+     * Note : as view can be long to build, we loop over timeout errors, until a
+     * result or another error is thrown.
+     * @param ref The reference to find usage for in database.
+     * @return List of found references.
+     */
+    private List<ReferenceUsage> getUsage(final ReferenceType ref) {
+        final ReferenceUsageRepository repo = Injector.getSession().getReferenceUsageRepository();
+        Throwable e = new TimeoutException();
+        while ((e instanceof TimeoutException || e instanceof InterruptedIOException) && !Thread.currentThread().isInterrupted()) {
+            try {
+                return repo.getReferenceUsages(ref.getId());
+            } catch (DbAccessException newException) {
+                final Throwable tmp = newException.getCause() == null? newException : newException.getCause();
+                tmp.addSuppressed(e);
+                e = tmp;
+            }
+        }
+
+        throw new DbAccessException(e);
+    }
 }
