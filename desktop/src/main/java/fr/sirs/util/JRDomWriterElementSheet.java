b8807138deac4c81709f1fc66803fc69a1f84dc0
Alexis Manin <alexis.manin@geomatys.com>
2016-10-21 13:06:38 +0200

    fix(desktop): fix printing of attribtute names on simple sheets.
    JIRA SYM-1361

--- desktop/src/main/java/fr/sirs/util/JRDomWriterElementSheet.java
+++ desktop/src/main/java/fr/sirs/util/JRDomWriterElementSheet.java
@@ -2,7 +2,7 @@
  * This file is part of SIRS-Digues 2.
  *
  * Copyright (C) 2016, FRANCE-DIGUES,
- * 
+ *
  * SIRS-Digues 2 is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
  * Software Foundation, either version 3 of the License, or (at your option) any
@@ -24,10 +24,7 @@ import fr.sirs.core.model.LabelMapper;
 import static fr.sirs.util.JRUtils.ATT_BACKCOLOR;
 import static fr.sirs.util.JRUtils.ATT_FONT_NAME;
 import static fr.sirs.util.JRUtils.ATT_HEIGHT;
-import static fr.sirs.util.JRUtils.ATT_IS_BOLD;
 import static fr.sirs.util.JRUtils.ATT_IS_STRETCH_WITH_OVERFLOW;
-import static fr.sirs.util.JRUtils.ATT_LINE_COLOR;
-import static fr.sirs.util.JRUtils.ATT_LINE_WIDTH;
 import static fr.sirs.util.JRUtils.ATT_MARKUP;
 import static fr.sirs.util.JRUtils.ATT_MODE;
 import static fr.sirs.util.JRUtils.ATT_POSITION_TYPE;
@@ -42,7 +39,6 @@ import fr.sirs.util.JRUtils.Markup;
 import fr.sirs.util.JRUtils.Mode;
 import fr.sirs.util.JRUtils.PositionType;
 import static fr.sirs.util.JRUtils.TAG_BAND;
-import static fr.sirs.util.JRUtils.TAG_BOTTOM_PEN;
 import static fr.sirs.util.JRUtils.TAG_BOX;
 import static fr.sirs.util.JRUtils.TAG_FONT;
 import static fr.sirs.util.JRUtils.TAG_FRAME;
@@ -79,16 +75,16 @@ import org.xml.sax.SAXException;
  * @author Samuel Andrés (Geomatys)
  */
 public class JRDomWriterElementSheet extends AbstractJDomWriterSingleSheet {
-    
-    
+
+
     // Dynamic template parameters.
     private int fields_interline;
-    
+
     // Static template parameters.
     private static final String FIELDS_VERTICAL_ALIGNMENT = "Middle";
     private static final String FIELDS_FONT_NAME = "Serif";
     private static final int FIELDS_FONT_SIZE = 8;
-    private static final int FIELDS_HEIGHT = 10;
+    private static final int FIELDS_HEIGHT = 13;
     //private static final String DATE_PATTERN = "dd/MM/yyyy à hh:mm:ss";
     private static final int INDENT_LABEL = 10;
     private static final int LABEL_WIDTH = 140;
@@ -99,26 +95,26 @@ public class JRDomWriterElementSheet extends AbstractJDomWriterSingleSheet {
     private static final int RIGHT_MARGIN = 20;
     private static final int TOP_MARGIN = 20;
     private static final int BOTTOM_MARGIN = 20;
-    
-    
+
+
     private JRDomWriterElementSheet(){
         super();
         fields_interline = 4;
     }
-    
+
     public JRDomWriterElementSheet(final InputStream stream) throws ParserConfigurationException, SAXException, IOException {
         super(stream);
         fields_interline = 4;
     }
-    
+
     /**
      * This setter changes the default fields interline.
-     * @param fieldsInterline 
+     * @param fieldsInterline
      */
     public void setFieldsInterline(int fieldsInterline){
         fields_interline = fieldsInterline;
     }
-    
+
     /**
      * <p>This method writes a Jasper Reports template mapping the parameter class.</p>
      * @param classToMap
@@ -127,16 +123,16 @@ public class JRDomWriterElementSheet extends AbstractJDomWriterSingleSheet {
      * @throws IOException
      */
     public void write(final Class classToMap, final List<String> avoidFields) throws TransformerException, IOException {
-        
+
         // Remove elements before inserting fields.-----------------------------
         root.removeChild(title);
         root.removeChild(pageHeader);
         root.removeChild(columnHeader);
         root.removeChild(detail);
-        
+
         // Modifies the template, based on the given class.---------------------
         this.writeObject(classToMap, avoidFields);
-        
+
         // Serializes the document.---------------------------------------------
         //DomUtilities.write(this.document, this.output);
         final Source source = new DOMSource(document);
@@ -152,10 +148,10 @@ public class JRDomWriterElementSheet extends AbstractJDomWriterSingleSheet {
      * <p>This method modifies the body of the DOM.</p>
      * @param classToMap
      * @param avoidFields field names to avoid.
-     * @throws Exception 
+     * @throws Exception
      */
     private void writeObject(final Class classToMap, final List<String> avoidFields) {
-        
+
         // Sets the initial fields used by the template.------------------------
         final Method[] methods = classToMap.getMethods();
         for (final Method method : methods){
@@ -166,27 +162,27 @@ public class JRDomWriterElementSheet extends AbstractJDomWriterSingleSheet {
                 }
             }
         }
-        
+
         // Modifies the title block.--------------------------------------------
         writeTitle("Fiche synoptique de ", classToMap);
-        
+
         // Writes the headers.--------------------------------------------------
         writePageHeader();
         writeColumnHeader();
-        
+
         // Builds the body of the Jasper Reports template.----------------------
         writeDetail(classToMap, avoidFields);
     }
-    
+
     /**
      * <p>This method writes the content of the detail element.</p>
      * @param classToMap
-     * @throws Exception 
+     * @throws Exception
      */
     private void writeDetail(final Class classToMap, List<String> avoidFields) {
-        
+
         final LabelMapper resourceBundle = LabelMapper.get(classToMap);
-        
+
         // Loops over the method looking for setters (based on the field names).
         final Method[] methods = classToMap.getMethods();
         final Collection<Method> setters = new ArrayList<>();
@@ -232,7 +228,7 @@ public class JRDomWriterElementSheet extends AbstractJDomWriterSingleSheet {
 
         int i = 0;
         for (final Method method : orderedSetters){
-                
+
             // Retrives the field name from the setter name.----------------
             final String fieldName = getFieldNameFromSetter(method);
             final Class fieldClass = method.getParameterTypes()[0];
@@ -251,38 +247,38 @@ public class JRDomWriterElementSheet extends AbstractJDomWriterSingleSheet {
                 i++;
             }
         }
-        
+
         // Sizes the detail element givent the field number.--------------------
         ((Element) this.detail.getElementsByTagName(TAG_BAND).item(0))
                 .setAttribute(ATT_HEIGHT, String.valueOf((FIELDS_HEIGHT+fields_interline)*i));
-        
+
         // Builds the DOM tree.-------------------------------------------------
         this.root.appendChild(this.detail);
     }
-    
+
     /**
      * <p>This method writes the variable of a given field.</p>
      * @param field
      * @param order
-     * @param heightMultiplicator 
+     * @param heightMultiplicator
      */
     private void writeDetailField(final String field, final Class fieldClass, final int order, final Markup markup, final LabelMapper resourceBundle){
-        
+
         // Looks for the band element.------------------------------------------
         final Element band = (Element) this.detail.getElementsByTagName(TAG_BAND).item(0);
-        
-        
+
+
         /*
         Sets the frame, that will contain the field label and the corresponding field value.
-        
+
         ------------------------------------------------------------------------
-        |                                                                      |   
-        |                               FRAME                                  |  
-        |                                                                      | 
+        |                                                                      |
+        |                               FRAME                                  |
+        |                                                                      |
         ------------------------------------------------------------------------
         */
         final Element frame = document.createElement(TAG_FRAME);
-        
+
         final Element frameReportElement = document.createElement(TAG_REPORT_ELEMENT);
         frameReportElement.setAttribute(ATT_X, String.valueOf(0));
         frameReportElement.setAttribute(ATT_Y, String.valueOf((FIELDS_HEIGHT+fields_interline)*order));
@@ -291,60 +287,53 @@ public class JRDomWriterElementSheet extends AbstractJDomWriterSingleSheet {
         frameReportElement.setAttribute(ATT_POSITION_TYPE, PositionType.FLOAT.toString());
         frameReportElement.setAttribute(ATT_MODE, Mode.OPAQUE.toString());
         if(order%2==0)
-            frameReportElement.setAttribute(ATT_BACKCOLOR, "#F0F0F0");
-        else
-            frameReportElement.setAttribute(ATT_BACKCOLOR, "#F5F5F5");
-        
+            frameReportElement.setAttribute(ATT_BACKCOLOR, "#F0F8FF");
+
         final Element box = document.createElement(TAG_BOX);
-        
-        final Element bottomPen = document.createElement(TAG_BOTTOM_PEN);
-        bottomPen.setAttribute(ATT_LINE_WIDTH, "0.25");
-        bottomPen.setAttribute(ATT_LINE_COLOR, "#CCCCCC");
-        
+
         // Builds the DOM tree.-------------------------------------------------
-        box.appendChild(bottomPen);
         frame.appendChild(frameReportElement);
         frame.appendChild(box);
-        
-        
+
+
         /*
         Sets the label, that will contain the field label.
-        
+
         ------------------------------------------------------------------------
-        |    --------------------  FRAME                                       |   
-        |    |       LABEL      |                                              |  
-        |    --------------------                                              | 
+        |    --------------------  FRAME                                       |
+        |    |       LABEL      |                                              |
+        |    --------------------                                              |
         ------------------------------------------------------------------------
         */
-        
+
         // Sets the field's label.----------------------------------------------
         final Element staticText = this.document.createElement(TAG_STATIC_TEXT);
-        
+
         final Element staticTextReportElement = this.document.createElement(TAG_REPORT_ELEMENT);
         staticTextReportElement.setAttribute(ATT_X, String.valueOf(INDENT_LABEL));
         staticTextReportElement.setAttribute(ATT_Y, String.valueOf(0));
         staticTextReportElement.setAttribute(ATT_WIDTH, String.valueOf(LABEL_WIDTH));
         staticTextReportElement.setAttribute(ATT_HEIGHT, String.valueOf(FIELDS_HEIGHT));
         staticTextReportElement.setAttribute(ATT_POSITION_TYPE, PositionType.FLOAT.toString());
-        
+
         final Element staticTextTextElement = this.document.createElement(TAG_TEXT_ELEMENT);
         staticTextTextElement.setAttribute(ATT_VERTICAL_ALIGNMENT, FIELDS_VERTICAL_ALIGNMENT);
         staticTextTextElement.setAttribute(ATT_TEXT_ALIGNMENT, TextAlignment.LEFT.toString());
-        
+
         final Element staticTextFont = this.document.createElement(TAG_FONT);
-        staticTextFont.setAttribute(ATT_IS_BOLD, "true");
+        staticTextFont.setAttribute(JRUtils.ATT_IS_BOLD, "true");
         staticTextFont.setAttribute(ATT_FONT_NAME, FIELDS_FONT_NAME);
         staticTextFont.setAttribute(ATT_SIZE, String.valueOf(FIELDS_FONT_SIZE));
-        
+
         final Element text = this.document.createElement(TAG_TEXT);
-        
+
         final CDATASection labelField;
         if(resourceBundle != null) {
             labelField = this.document.createCDATASection(resourceBundle.mapPropertyName(field));
         } else{
             labelField = this.document.createCDATASection(field);
         }
-        
+
         // Builds the DOM tree.-------------------------------------------------
         text.appendChild(labelField);
         staticText.appendChild(staticTextReportElement);
@@ -352,16 +341,16 @@ public class JRDomWriterElementSheet extends AbstractJDomWriterSingleSheet {
         staticText.appendChild(staticTextTextElement);
         staticText.appendChild(text);
         frame.appendChild(staticText);
-        
-        
-        
+
+
+
         /*
         Sets the field, that will contain the field value.
-        
+
         ------------------------------------------------------------------------
-        |    --------------------  FRAME  -------------------------------------|   
-        |    |       LABEL      |         |               FIELD               ||  
-        |    --------------------         -------------------------------------| 
+        |    --------------------  FRAME  -------------------------------------|
+        |    |       LABEL      |         |               FIELD               ||
+        |    --------------------         -------------------------------------|
         ------------------------------------------------------------------------
         */
         // Sets the field.------------------------------------------------------
@@ -378,25 +367,25 @@ public class JRDomWriterElementSheet extends AbstractJDomWriterSingleSheet {
         textFieldReportElement.setAttribute(ATT_WIDTH, String.valueOf(COLUMN_WIDTH-(INDENT_LABEL+LABEL_WIDTH)));
         textFieldReportElement.setAttribute(ATT_HEIGHT, String.valueOf(FIELDS_HEIGHT));
         textFieldReportElement.setAttribute(ATT_POSITION_TYPE, PositionType.FLOAT.toString());
-        
+
         final Element textFieldTextElement = document.createElement(TAG_TEXT_ELEMENT);
         textFieldTextElement.setAttribute(ATT_VERTICAL_ALIGNMENT, FIELDS_VERTICAL_ALIGNMENT);
         textFieldTextElement.setAttribute(ATT_TEXT_ALIGNMENT, TextAlignment.JUSTIFIED.toString());
-        if(markup!=null && markup!=Markup.NONE) 
+        if(markup!=null && markup!=Markup.NONE)
             textFieldTextElement.setAttribute(ATT_MARKUP, markup.toString());
-        
+
         final Element textFieldFont = document.createElement(TAG_FONT);
         textFieldFont.setAttribute(ATT_FONT_NAME, FIELDS_FONT_NAME);
         textFieldFont.setAttribute(ATT_SIZE, String.valueOf(FIELDS_FONT_SIZE));
-        
+
         final Element textFieldExpression = document.createElement(TAG_TEXT_FIELD_EXPRESSION);
-        
+
         // The content of the field is specific in case of Calendar field.------
         final CDATASection valueField;
-        //if (c==Instant.class) 
+        //if (c==Instant.class)
         //    valueField = this.document.createCDATASection("$F{"+field+"}");
         //else $F{permit_quantity}.equals(null) ? $F{fst_insp_qpqlml_quantity} : $F{permit_quantity}
-        
+
         if(fieldClass==Boolean.class || (fieldClass!=null && BOOLEAN_PRIMITIVE_NAME.equals(fieldClass.getName()))){
             if(BORNE_DEBUT_AVAL.equals(field) || BORNE_FIN_AVAL.equals(field)){
                 valueField = document.createCDATASection("$F{"+field+"}==null ? \""+NULL_REPLACEMENT+"\" : ($F{"+field+"} ? \"Amont\" : \"Aval\")");
@@ -408,7 +397,7 @@ public class JRDomWriterElementSheet extends AbstractJDomWriterSingleSheet {
         else{
             valueField = document.createCDATASection("$F{"+field+"}==null ? \""+NULL_REPLACEMENT+"\" : $F{"+field+"}");
         }
-        
+
         // Builds the DOM tree.-------------------------------------------------
         textFieldExpression.appendChild(valueField);
         textField.appendChild(textFieldReportElement);
@@ -416,7 +405,7 @@ public class JRDomWriterElementSheet extends AbstractJDomWriterSingleSheet {
         textField.appendChild(textFieldTextElement);
         textField.appendChild(textFieldExpression);
         frame.appendChild(textField);
-        
+
         band.appendChild(frame);
     }
 }
