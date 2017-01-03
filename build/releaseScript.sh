90cddeb18fb4b8d4409a8b22d8c0731f9e8e21df
Alexis Manin <alexis.manin@geomatys.com>
2017-01-03 12:36:56 +0100

    Prepare release script for next build.

--- build/releaseScript.sh
+++ build/releaseScript.sh
@@ -21,7 +21,7 @@
 
 ############## DEFAULT VALUES TO USE FOR RELEASE VERSION 								 ##############
 ############## WARNING : DO NOT AFFECT THOSE VARIABLES IN ANOTHER PLACE OF THIS SCRIPT ! ##############
-defaultMinor=4
+defaultMinor=5
 defaultMajor=2
 
 
