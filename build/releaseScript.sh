7ed6fd8b0f4a5fdaa998afaa100f504be24c6a18
Samuel Andrés <samuel.andres@geomatys.com>
2018-09-25 08:56:39 +0200

    Prepare release script for next build.

--- build/releaseScript.sh
+++ build/releaseScript.sh
@@ -21,7 +21,7 @@
 
 ############## DEFAULT VALUES TO USE FOR RELEASE VERSION 								 ##############
 ############## WARNING : DO NOT AFFECT THOSE VARIABLES IN ANOTHER PLACE OF THIS SCRIPT ! ##############
-defaultMinor=13
+defaultMinor=14
 defaultMajor=2
 
 
