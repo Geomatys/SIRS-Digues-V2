b2f6da2ad68cd3d26c4012972592fbe30e898bb8
Samuel Andrés <samuel.andres@geomatys.com>
2019-01-30 10:31:27 +0100

    fix(plugin-carto): do not include BookMarkTheme pane into pane cache (SYM-1836)

--- plugin-carto/src/main/java/fr/sirs/plugin/carto/BookMarkTheme.java
+++ plugin-carto/src/main/java/fr/sirs/plugin/carto/BookMarkTheme.java
@@ -2,7 +2,7 @@
  * This file is part of SIRS-Digues 2.
  *
  * Copyright (C) 2016, FRANCE-DIGUES,
- * 
+ *
  * SIRS-Digues 2 is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
  * Software Foundation, either version 3 of the License, or (at your option) any
@@ -38,5 +38,7 @@ public final class BookMarkTheme extends AbstractPluginsButtonTheme {
     public Parent createPane() {
         return new FXBookMarks();
     }
-    
+
+    @Override
+    public boolean isCached(){return false;}
 }
