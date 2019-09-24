b8939821a2db732f669ca15c386839de192a165d
Matthieu_Bastianelli <matthieu.bastianelli@geomatys.com>
2019-09-24 10:06:35 +0200

    Prepare release script for next build.

--- build/releaseScript.sh
+++ build/releaseScript.sh
@@ -21,7 +21,7 @@
 
 ############## DEFAULT VALUES TO USE FOR RELEASE VERSION 								 ##############
 ############## WARNING : DO NOT AFFECT THOSE VARIABLES IN ANOTHER PLACE OF THIS SCRIPT ! ##############
-defaultMinor=22
+defaultMinor=23
 defaultMajor=2
 
 
