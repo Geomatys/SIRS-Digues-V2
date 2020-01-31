9e1f3b465d9bb499a56cb100b5424d6b3eacbd1c
Matthieu_Bastianelli <matthieu.bastianelli@geomatys.com>
2020-01-31 17:56:12 +0100

    feat (launcher upgrade1NtoNN) : try to generate a report of potential exceptions.

--- desktop/src/main/java/fr/sirs/migration/upgrade/v2and23/UpgradeLink1NtoNN.java
+++ desktop/src/main/java/fr/sirs/migration/upgrade/v2and23/UpgradeLink1NtoNN.java
@@ -36,6 +36,17 @@ public final class UpgradeLink1NtoNN extends Task {
     private final String dbName;
     private final Upgrades1NtoNNSupported upgrade;
 
+    /**
+     * Attribut représentant l'ensemble des échecs durant la mise à jour.
+     * Consiste en une Map.
+     * Les clefs sont les éléments portant initialement la relation mise à jour en échec.
+     * Les valeurs sont les identifiants (String) des éléments associé à la clef
+     * qui n'ont pas pu être mis à jour.
+     *
+     */
+    private final UpgradeFailureReport failureReport = new UpgradeFailureReport();
+
+
     public UpgradeLink1NtoNN(final DatabaseRegistry dbRegistry, final String dbName, final Upgrades1NtoNNSupported upgrade){
         ArgumentChecks.ensureNonNull("dbRegistry", dbRegistry);
         ArgumentChecks.ensureNonNull("dbName", dbName);
@@ -51,7 +62,11 @@ public final class UpgradeLink1NtoNN extends Task {
     @Override
     protected Object call() throws Exception {
 
+        updateTitle(String.format("Mise à jours des relations 1-N en relation N-N pour la version : %d.%d ", upgrade.upgradeMajorVersion, upgrade.upgradeMinorVersion));
+
+        updateMessage("Connection à la base de données");
         try (final ConfigurableApplicationContext upgradeContext = this.dbRegistry.connectToSirsDatabase(dbName, false, false, false)) {
+
 
             Session session = upgradeContext.getBean(Session.class);
 
@@ -67,7 +82,8 @@ public final class UpgradeLink1NtoNN extends Task {
                             c -> session.getRepositoryForClass(c)
                     ));
 
-            updateTitle(String.format("Mise à jours des relations 1-N en relation N-N pour la version : %d.%d ", upgrade.upgradeMajorVersion, +upgrade.upgradeMinorVersion));
+
+            updateMessage("Parcours des éléments portant initialement les liens et mise à jour");
 
             /* Parcours des éléments portant initialement les liens
             * (exemple : si migration [Desordre 1..* Ouvrage] en [Desordre *..* Ouvrage]
@@ -85,12 +101,25 @@ public final class UpgradeLink1NtoNN extends Task {
         } catch (Exception ex) {
             LOGGER.log(Level.WARNING, ex.getMessage(), ex);
             Platform.runLater(() -> GeotkFX.newExceptionDialog("Une erreur est survenue pendant l'import de la base.", ex).showAndWait());
+
+        } finally {
+            updateMessage("Fin de la mise à jour; Lecture du rapport d'erreurs");
+            final String message =
+                  "\n\n====================================================="
+                    + "Fin de la mise à jour; Lecture du rapport d'erreurs :"
+                    + "=====================================================\n";
+            LOGGER.log(Level.WARNING, message);
+            LOGGER.log(Level.WARNING, failureReport.getStringReport());
+
         }
 
         return false;
     }
 
     private void upgrade(final Upgrades1NtoNNSupported upgrade, final Element element, final Map< Class, AbstractSIRSRepository<Element>> reposSidesN) {
+
+        updateMessage(String.format("Element parcouru : %s   -> %s ", element.getClass().getCanonicalName(), element.getDesignation()));
+
         if (!(upgrade.linkSide1.isInstance(element))) {
             throw new IllegalArgumentException(" Element input must be an instance of the upgrade input");
         }
@@ -101,17 +130,35 @@ public final class UpgradeLink1NtoNN extends Task {
                 final AbstractSIRSRepository<Element> repo = reposSidesN.get(classeAndGetter.clazz);
                 final List<Element> updated = extractedIds.stream()
                         .map(
-                                id -> {
-                                    final Element elt = repo.get(id);
-                                    elt.addChild(element);
-                                    return elt;
-                                })
+                                id -> addChildToElementFromId(element, id, repo) //return null if an exception occured when trying to add child
+                        )
+                        .filter(e -> e != null)
                         .collect(Collectors.toList());
                 repo.executeBulk(updated);
 
             }
         } catch (Exception e) {
             throw new BackingStoreException(e.getCause());
+        }
+    }
+
+    /**
+     *
+     * @param element to add as child
+     * @param id id of the {@link Element} we want to add a child
+     * @param repo repository allowing to retrive the {@link Element} associated with the id-input
+     * @return the {@link Element} associated with the given input after adding the input {@linkplain Element element} Or null if an exception occured.
+     * The null return aim to allow to filter the updated elements.
+     */
+    private Element addChildToElementFromId(final Element element, final String id, final AbstractSIRSRepository<Element> repo) {
+        try {
+            final Element elt = repo.get(id);
+            elt.addChild(element);
+            return elt;
+        } catch (Exception e) {
+            final String message = failureReport.addFailure(element,id);
+            LOGGER.log(Level.WARNING, message, e);
+            return null;
         }
     }
 
