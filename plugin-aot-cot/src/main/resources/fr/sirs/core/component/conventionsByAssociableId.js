32d2a37efab5c8a56621dbcd8abb164e890efd27
Maxime Gavens <maximegavens@localhost.localdomain>
2021-05-24 11:57:18 +0200

    fix(plugin-aot-cot): solves the problem of not opening sirs when the aot-cot plugin is installed, due to an error in the script

--- plugin-aot-cot/src/main/resources/fr/sirs/core/component/conventionsByAssociableId.js
+++ plugin-aot-cot/src/main/resources/fr/sirs/core/component/conventionsByAssociableId.js
@@ -1,6 +1,6 @@
 function(doc) {
     if(doc['@class']=='fr.sirs.core.model.Convention'){
-        for each (association in doc.associations){
+        for (const association in doc.associations){
            emit(association.objetId, doc);
         }
     }
