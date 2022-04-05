4003325c57ae3ca4fd9c33a78965b179f433ce8f
maximegavens <maxime.gavens@gmail.com>
2022-04-05 15:46:19 +0200

    Prepare release script for next build.

--- build/releaseScript.sh
+++ build/releaseScript.sh
@@ -21,7 +21,7 @@
 
 ############## DEFAULT VALUES TO USE FOR RELEASE VERSION 								 ##############
 ############## WARNING : DO NOT AFFECT THOSE VARIABLES IN ANOTHER PLACE OF THIS SCRIPT ! ##############
-defaultMinor=38
+defaultMinor=37
 defaultMajor=2
 
 
