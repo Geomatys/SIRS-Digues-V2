17e733baf5e6285de285b55af3930b0b64adaf46
eidee <estelle.idee@geomatys.com>
2023-12-18 14:35:29 +0100

    feat(berge): modified code for FXBergeThemePane to extend FXAbstractTronconThemePane.

--- plugin-berge/src/main/java/fr/sirs/plugin/berge/ui/AbstractDescriptionPane.java
+++ plugin-berge/src/main/java/fr/sirs/plugin/berge/ui/AbstractDescriptionPane.java
@@ -1,28 +1,27 @@
 /**
  * This file is part of SIRS-Digues 2.
- *
+ * <p>
  * Copyright (C) 2016, FRANCE-DIGUES,
- * 
+ * <p>
  * SIRS-Digues 2 is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
  * Software Foundation, either version 3 of the License, or (at your option) any
  * later version.
- *
+ * <p>
  * SIRS-Digues 2 is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
  * details.
- *
+ * <p>
  * You should have received a copy of the GNU General Public License along with
  * SIRS-Digues 2. If not, see <http://www.gnu.org/licenses/>
  */
 package fr.sirs.plugin.berge.ui;
 
-import com.sun.javafx.binding.Logging;
 import fr.sirs.SIRS;
 import fr.sirs.core.model.AvecForeignParent;
-import fr.sirs.core.model.Berge;
 import fr.sirs.core.model.PositionDocument;
+import fr.sirs.core.model.Preview;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
 import javafx.scene.layout.BorderPane;
@@ -38,7 +37,7 @@ import java.util.List;
 public class AbstractDescriptionPane extends BorderPane {
 
     @FXML
-    private ComboBox<Berge> bergeBox;
+    private ComboBox<Preview> bergeBox;
     
     @FXML
     private BorderPane uiCenter;
