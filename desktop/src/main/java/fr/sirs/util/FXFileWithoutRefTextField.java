1f8962272f7e7f4e4d30cf86711094b5e13f712e
eidee <estelle.idee@geomatys.com>
2024-09-09 12:38:56 +0200

    fix(desktop): fix path error message not working properly in FXPhotoPane.

--- desktop/src/main/java/fr/sirs/util/FXFileWithoutRefTextField.java
+++ desktop/src/main/java/fr/sirs/util/FXFileWithoutRefTextField.java
@@ -1,18 +1,18 @@
 /**
  * This file is part of SIRS-Digues 2.
- *
+ * <p>
  * Copyright (C) 2016, FRANCE-DIGUES,
- * 
+ * <p>
  * SIRS-Digues 2 is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
  * Software Foundation, either version 3 of the License, or (at your option) any
  * later version.
- *
+ * <p>
  * SIRS-Digues 2 is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
  * details.
- *
+ * <p>
  * You should have received a copy of the GNU General Public License along with
  * SIRS-Digues 2. If not, see <http://www.gnu.org/licenses/>
  */
@@ -115,7 +115,12 @@ public class FXFileWithoutRefTextField extends AbstractPathTextField {
                 }
             }
 
-            final File syntheseFile = new File(text);
+            final File syntheseFile;
+            try {
+                syntheseFile = new File(getURIForText(text));
+            } catch (Exception e) {
+                throw new IllegalStateException("Error while retrieving URI for text " + text, e);
+            }
             if (!syntheseFile.exists()) {
                 ui_errorMessage.setText("Le fichier est introuvable.");
                 return;
