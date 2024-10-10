5f86697d349a9ad1419bb4328d7b244a7bef4ebb
eidee <estelle.idee@geomatys.com>
2024-10-10 12:28:22 +0200

    Prepare release script for next build.

--- build/releaseScript.sh
+++ build/releaseScript.sh
@@ -21,7 +21,7 @@
 
 ############## DEFAULT VALUES TO USE FOR RELEASE VERSION 								 ##############
 ############## WARNING : DO NOT AFFECT THOSE VARIABLES IN ANOTHER PLACE OF THIS SCRIPT ! ##############
-defaultMinor=49
+defaultMinor=50
 defaultMajor=2
 
 
